e7cec739be3047cc9470e4b01004807bc2ae79ab
a.boyko <a.boyko@gaijin.ru>
2013-10-08 17:47:34 +0300

    settings for Little Mermaid (for screens, blocks & small blocks editors, not for enemy & layout)

---
+++ CadEditor/Settings_Mermaid-1.cs
@@ -0,0 +1,47 @@
+using CadEditor;
+using System.Collections.Generic;
+
+public class Data
+{
+  //Using types
+  //OffsetRec
+  //public delegate int GetVideoPageAddrFunc(int videoPageId);
+  //public delegate byte[] GetVideoChunkFunc(int videoPageId);
+  //public delegate void SetVideoChunkFunc(int videoPageId, byte[] videoChunk);
+  /*public struct LevelRec
+  {
+      public LevelRec(int objectsBeginAddr, int objCount, int width = 0, int height = 0, int layoutAddr = 0)
+      {
+          this.objCount = objCount;
+          this.objectsBeginAddr = objectsBeginAddr;
+          this.width = width;
+          this.height = height;
+          this.layoutAddr = layoutAddr;
+      }
+      public int objCount;
+      public int objectsBeginAddr;
+      public int width;
+      public int height;
+      public int layoutAddr;
+  }*/
+  
+  public static GameType getGameType()  { return GameType.Generic; }
+  
+  public static OffsetRec getPalOffset()       { return new OffsetRec(0x1DB53, 32  , 16);  }
+  public static OffsetRec getVideoOffset()     { return new OffsetRec(0xC010, 1 , 0x1000); }
+  public static OffsetRec getVideoObjOffset()  { return new OffsetRec(0xC010, 1 , 0x1000); }
+  public static OffsetRec getBigBlocksOffset() { return new OffsetRec(0x510 , 1 , 0x4000); }
+  public static OffsetRec getBlocksOffset()    { return new OffsetRec(0x10 ,  1 , 0x4000); }
+  public static OffsetRec getScreensOffset()   { return new OffsetRec(0x4010,64 , 0x40);   }
+  public static int getBigBlocksCount()        { return 256; }
+  public static IList<LevelRec> getLevelRecs() { return levelRecs; }
+  
+  public static GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public static GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public static SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  
+  public static IList<LevelRec> levelRecs = new List<LevelRec>() 
+  {
+    new LevelRec(0x10000, 1, 8, 8,  0x1DAA8), 
+  };
+}
---
+++ CadEditor/Settings_Mermaid-4.cs
@@ -0,0 +1,56 @@
+using CadEditor;
+using System.Collections.Generic;
+
+public class Data
+{
+  //Using types
+  //OffsetRec
+  //public delegate int GetVideoPageAddrFunc(int videoPageId);
+  //public delegate byte[] GetVideoChunkFunc(int videoPageId);
+  //public delegate void SetVideoChunkFunc(int videoPageId, byte[] videoChunk);
+  /*public struct LevelRec
+  {
+      public LevelRec(int objectsBeginAddr, int objCount, int width = 0, int height = 0, int layoutAddr = 0)
+      {
+          this.objCount = objCount;
+          this.objectsBeginAddr = objectsBeginAddr;
+          this.width = width;
+          this.height = height;
+          this.layoutAddr = layoutAddr;
+      }
+      public int objCount;
+      public int objectsBeginAddr;
+      public int width;
+      public int height;
+      public int layoutAddr;
+  }*/
+  
+  public static GameType getGameType()  { return GameType.Generic; }
+  
+  public static OffsetRec getPalOffset()       { return new OffsetRec(0x1DB53, 32  , 16);  }
+  public static OffsetRec getVideoOffset()     { return new OffsetRec(0xE410, 1 , 0xD00); }
+  public static OffsetRec getVideoObjOffset()  { return new OffsetRec(0xE410, 1 , 0xD00); }
+  public static OffsetRec getBigBlocksOffset() { return new OffsetRec(0x2010, 1 , 0x4000); }
+  public static OffsetRec getBlocksOffset()    { return new OffsetRec(0x1B10,  1 , 0x4000); }
+  public static OffsetRec getScreensOffset()   { return new OffsetRec(0x4DD0, 32 , 0x40);   }
+  public static int getBigBlocksCount()        { return 256; }
+  public static IList<LevelRec> getLevelRecs() { return levelRecs; }
+  
+  public static GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public static GetVideoChunkFunc    getVideoChunkFunc()    { return getLMVideoChunk; }
+  public static SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  
+  public static IList<LevelRec> levelRecs = new List<LevelRec>() 
+  {
+    new LevelRec(0x10000, 1, 8, 8,  0x1DAE2), 
+  };
+  
+  public static byte[] getLMVideoChunk(int videoPageId)
+  {
+    byte[] videoChunk = Utils.getVideoChunk(videoPageId);
+    //fill first quarter of videoChunk with constant to all video memory data
+    for (int i = 0; i < 16 * 16 * 4; i++)
+        videoChunk[i] = Globals.romdata[0xC010 + i];
+    return videoChunk;
+  }
+}
