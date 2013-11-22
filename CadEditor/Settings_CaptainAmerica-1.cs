4ce1f2a060885d89515a0b2d94aecbb47948bff9
a.boyko <a.boyko@gaijin.ru>
2013-11-22 18:11:58 +0200

    settings for Super C (KC) & Captain America, levels 1

---
+++ CadEditor/Settings_CaptainAmerica-1.cs
@@ -0,0 +1,18 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public GameType getGameType()        { return GameType.Generic; }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(8208, 7 , 16*15);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 15; }
+  public string getBlocksFilename()    { return "captain_america_1.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
