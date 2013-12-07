387ca422954642bdb15937960f91a32723ee2fd9
a.boyko <a.boyko@gaijin.ru>
2013-12-07 15:15:41 +0200

    remove old xml configs settings, pictures & dumps for Sega Tiny Toon Buster's Hidden Treasure, levels 1-1 - 1-4

---
+++ CadEditor/Settings_SegaTinyToon-1_layerB.cs
@@ -0,0 +1,18 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public GameType getGameType()        { return GameType.Generic; }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0, 320 , 8*1);   }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 1; }
+  public string getBlocksFilename()    { return "sega_tiny_toon_1b.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
