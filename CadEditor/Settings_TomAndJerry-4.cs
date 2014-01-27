207c3bf97c72c4c0b78cbaf9a2fce2f9f12ecb4f
a.boyko <a.boyko@gaijin.ru>
2014-01-27 13:21:47 +0200

    tom and jerry level 1-8 configs, enemies for level 1

---
+++ CadEditor/Settings_TomAndJerry-4.cs
@@ -0,0 +1,22 @@
+using CadEditor;
+using System.Collections.Generic;
+//css_include Settings_CapcomBase.cs;
+public class Data : CapcomBase
+{ 
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 32  , 16);     }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0, 16  , 0x1000); }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0, 16  , 0x1000); }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0 , 8   , 0x4000); }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0 , 8   , 0x4000); }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(57874   , 1 , 32*127);   }
+  public override int getScreenWidth()    { return 32; }
+  public override int getScreenHeight()   { return 127; }
+  public string getBlocksFilename() { return "tom_and_jerry_4.png"; }
+  public IList<LevelRec> getLevelRecs() { return null; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
