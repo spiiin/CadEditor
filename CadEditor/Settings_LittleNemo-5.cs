ca9530baaf1ddef53d1ae4ed3f8baae982e7139a
a.boyko <a.boyko@gaijin.ru>
2014-01-28 17:49:21 +0200

    settings for little nemo, levels 2-8

---
+++ CadEditor/Settings_LittleNemo-5.cs
@@ -0,0 +1,18 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public GameType getGameType()        { return GameType.Generic; }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(35088, 33 , 8*8);   }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 8; }
+  public string getBlocksFilename()    { return "little_nemo_5.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
