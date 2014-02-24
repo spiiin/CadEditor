f4fc4bb8561b26f06aad380363c9d79a27c9c455
a.boyko <a.boyko@gaijin.ru>
2014-02-24 18:33:48 +0200

    v2.5

--- CadEditor/FormMain.Designer.cs
+++ CadEditor/FormMain.Designer.cs
@@ -747,7 +747,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "FormMain";
-            this.Text = "CAD Editor v2.4.1 by spiiin";
+            this.Text = "CAD Editor v2.5 by spiiin";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormMain_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.mapScreen)).EndInit();
