124a1385c47ce6de4bb95b82b4fae2251cd5bdd5
a.boyko <a.boyko@gaijin.ru>
2014-03-20 16:26:16 +0200

    tmnt 2, level 3

---
+++ CadEditor/settings_tmnt2/Settings_Tmnt2-3.cs
@@ -0,0 +1,18 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public GameType getGameType()       { return GameType.Generic; }
+  public OffsetRec getScreensOffset() { return new OffsetRec(0xD70D, 8, 48); }
+  public int getScreenWidth()         { return 8; }
+  public int getScreenHeight()        { return 6; }
+  public string getBlocksFilename()   { return "settings_tmnt2/tmnt2_3.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
