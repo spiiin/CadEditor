309cd595f115eea6b75d5286876031350018aa58
a.boyko <a.boyko@gaijin.ru>
2014-03-31 19:03:39 +0300

    settings for levels 3,4

---
+++ CadEditor/settings_tmnt3/Settings_Tmnt3-4-2.cs
@@ -0,0 +1,18 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public GameType getGameType()       { return GameType.Generic; }
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x2AB19, 2, 8*6); }
+  public int getScreenWidth()         { return 8; }
+  public int getScreenHeight()        { return 6; }
+  public string getBlocksFilename()   { return "settings_tmnt3/tmnt3_4-2.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
