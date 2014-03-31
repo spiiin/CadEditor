7911ccfd03a3fbb81121ea579cd884ad87a065a2
a.boyko <a.boyko@gaijin.ru>
2014-03-31 16:47:09 +0300

    settings for ffta, 2 layers at once, clear picture with blocks

--- CadEditor/settings_gba_final_fantasy_tactics_advance/Settings_GbaFfta.cs
+++ CadEditor/settings_gba_final_fantasy_tactics_advance/Settings_GbaFfta.cs
@@ -5,13 +5,15 @@ using System.Drawing;
 public class Data 
 { 
   public GameType getGameType()        { return GameType.Generic; }
-  public OffsetRec getScreensOffset()  { return new OffsetRec(0x0, 1 , 0x20*0x40);   }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(4096, 1 , 0x20*0x40);   }
+  public OffsetRec getScreensOffset2() { return new OffsetRec(0   , 1 , 0x20*0x40);   }
   public int getScreenWidth()          { return 0x20; }
   public int getScreenHeight()         { return 0x40; }
   public int getWordLen()              { return 2;}
+  public int getLayersCount()          { return 2;}
   public bool isLittleEndian()         { return true; }
   public int    getPictureBlocksWidth(){ return 64; }
-  public int getBigBlocksCount()       { return 576; }
+  public int getBigBlocksCount()       { return 542; }
   public string getBlocksFilename()    { return "settings_gba_final_fantasy_tactics_advance/gba_ffta_1.png"; }
   
   public bool isBigBlockEditorEnabled() { return false; }
