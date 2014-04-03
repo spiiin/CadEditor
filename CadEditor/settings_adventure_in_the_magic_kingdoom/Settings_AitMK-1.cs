784d4fcf7dc4e37f025e6efdbf3ce061d4f427be
a.boyko <a.boyko@gaijin.ru>
2014-04-03 18:14:00 +0300

    adventure in the magic kingdom, level castle begin

---
+++ CadEditor/settings_adventure_in_the_magic_kingdoom/Settings_AitMK-1.cs
@@ -0,0 +1,16 @@
+using CadEditor;
+using System.Collections.Generic;
+public class Data
+{ 
+  public GameType getGameType()           { return GameType.Generic; }
+  public OffsetRec getScreensOffset()     { return new OffsetRec(0xA890, 4 /*next - castle level too, with other graphics*/, 8*9);   }
+  public int getScreenWidth()             { return 8; }
+  public int getScreenHeight()            { return 9; }
+  public string getBlocksFilename()       { return "adv_1.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
