7cec2c7353720ff52425392d70c612408aea1b6c
a.boyko <a.boyko@gaijin.ru>
2014-05-01 18:10:02 +0400

    flintstones macroblocks editor, level 1

---
+++ CadEditor/settings_flintstones_rescue_of_dino_and_hoppy/Settings_Flintstones-Blocks-1.cs
@@ -0,0 +1,17 @@
+using CadEditor;
+using System.Collections.Generic;
+public class Data
+{ 
+  public GameType getGameType()           { return GameType.TT; }
+  public OffsetRec getScreensOffset()     { return new OffsetRec(0x1F0, 256 , 4*4);   }
+  public int getScreenWidth()             { return 4; }
+  public int getScreenHeight()            { return 4; }
+  public bool getScreenVertical()         { return true; }
+  public string getBlocksFilename()       { return "flintstones_blocks_1.png"; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+}
