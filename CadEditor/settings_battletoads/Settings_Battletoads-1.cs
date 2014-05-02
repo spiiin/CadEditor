67b61405b5126cd83de8470f2f5e37dbb0601dcc
a.boyko <a.boyko@gaijin.ru>
2014-05-02 16:17:34 +0400

    render collision layer for battletoads

--- CadEditor/settings_battletoads/Settings_Battletoads-1.cs
+++ CadEditor/settings_battletoads/Settings_Battletoads-1.cs
@@ -1,5 +1,8 @@
 using CadEditor;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
+
 public class Data
 { 
   public GameType getGameType()  { return GameType.Generic; }
@@ -14,4 +17,27 @@ public class Data
   public bool isLayoutEditorEnabled()   { return false; }
   public bool isEnemyEditorEnabled()    { return false; }
   public bool isVideoEditorEnabled()    { return false; }
+  
+  public RenderToMainScreenFunc getRenderToMainScreenFunc() { return renderObjects; }
+  
+  public void renderObjects(Graphics g, int curScale)
+  {
+    for (int i = 0; i < 64; i++)
+    {
+        byte b1  = Globals.romdata[0x2BE4D + i*6];
+        byte b2  = Globals.romdata[0x2BE4D + i*6 + 1];
+        byte b3  = Globals.romdata[0x2BE4D + i*6 + 2];
+        byte b4  = Globals.romdata[0x2BE4D + i*6 + 3];
+        byte b5  = Globals.romdata[0x2BE4D + i*6 + 4];
+        byte b6  = Globals.romdata[0x2BE4D + i*6 + 5];
+        var rect = new Rectangle(32*curScale*(i+1), 8*32*curScale, 32*curScale*(i+2), (curScale*32*8)+80);
+        g.DrawRectangle(new Pen(Color.Red, 4.0f), rect);
+        g.DrawString(String.Format("{0:X2}", b1), new Font("Arial", 8), Brushes.Red, rect);
+        g.DrawString(String.Format("{0:X2}", b2), new Font("Arial", 8), Brushes.Red, rect.X + 16, rect.Y);
+        g.DrawString(String.Format("{0:X2}", b3), new Font("Arial", 8), Brushes.Red, rect.X + 32, rect.Y);
+        g.DrawString(String.Format("{0:X2}", b4), new Font("Arial", 8), Brushes.Red, rect.X + 0 , rect.Y+16);
+        g.DrawString(String.Format("{0:X2}", b5), new Font("Arial", 8), Brushes.Red, rect.X + 16, rect.Y+16);
+        g.DrawString(String.Format("{0:X2}", b6), new Font("Arial", 8), Brushes.Red, rect.X + 32, rect.Y+16);
+    }
+  }
 }
