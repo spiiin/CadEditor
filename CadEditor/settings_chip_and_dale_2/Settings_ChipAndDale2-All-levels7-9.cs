1468267f79213882f378224a2cd3e120432a7240
a.boyko <a.boyko@gaijin.ru>
2014-06-04 14:09:10 +0400

    settings for chip & dale prizes, levels 7-8

--- CadEditor/settings_chip_and_dale_2/Settings_ChipAndDale2-All-levels7-9.cs
+++ CadEditor/settings_chip_and_dale_2/Settings_ChipAndDale2-All-levels7-9.cs
@@ -1,26 +1,31 @@
 using CadEditor;
 using System.Collections.Generic;
 //css_include Settings_CapcomBase.cs;
+
+//Changes for levels 7,8,9:
+// screensOffset
+// palOffset
+
 public class Data : CapcomBase
 { 
-  public override GameType getGameType()  { return GameType.DT2; }
-  public OffsetRec getPalOffset()       { return new OffsetRec(0xF12E  , 32  , 16);     }
-  public OffsetRec getVideoOffset()     { return new OffsetRec(0x20010 , 32  , 0x1000); } //15 for 1st level
+  public override GameType getGameType(){ return GameType.DT2; }
+  public OffsetRec getPalOffset()       { return new OffsetRec(0xF291 - 16  , 32  , 16);     }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0x20010 , 32  , 0x1000); }
   public OffsetRec getVideoObjOffset()  { return new OffsetRec(0, 16   , 0x1000); }
   public OffsetRec getBigBlocksOffset() { return new OffsetRec(0 , 8   , 0x4000); }
   public OffsetRec getBlocksOffset()    { return new OffsetRec(0xC010  , 8   , 0x440); }
-  public OffsetRec getScreensOffset()   { return new OffsetRec(0x210   , 256 , 16*15);   }
-  public override int getScreenWidth()    { return 16; }
-  public override int getScreenHeight()   { return 15; }
-  public IList<LevelRec> getLevelRecs() { return levelRecsCad2; }
-  public int getScrollsOffsetFromLayout() { return 538; } //offset scrolls array from layout array
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0x210 + 32, 256 , 16*15);   }
+  public override int getScreenWidth()     { return 16; }
+  public override int getScreenHeight()    { return 15; }
+  public IList<LevelRec> getLevelRecs()    { return levelRecsCad2; }
+  public int getScrollsOffsetFromLayout()  { return 538; } //offset scrolls array from layout array
   public GetObjectsFunc getObjectsFunc()   { return getObjectsCad2; }
   public SetObjectsFunc setObjectsFunc()   { return setObjectsCad2; }
   public override GetVideoPageAddrFunc getVideoPageAddrFunc() { return getChrAddress; }
   public bool isBuildScreenFromSmallBlocks() { return true; }
-  public GetLayoutFunc getLayoutFunc()     { return getLayout;   }
+  public GetLayoutFunc getLayoutFunc()       { return getLayout;   }
   
-  const int LEVEL_REC_COUNT = 30;
+  const int LEVEL_REC_COUNT = 15;
   
   LevelLayerData getLayout(int levelNo)
   {
@@ -34,57 +39,20 @@ public class Data : CapcomBase
   {
     var levelLayers = new byte[][]
     {
-        new byte[] { 5,6,7,5,6,7,5,6 },                                     //1-1
-        new byte[] { 1, 0xC, 0x12, 1, 0xC, 0x13, 0x10, 0x11, 0xF },         //1-2
-        new byte[] { 9, 0xA, 0xB, 0xC, 0xA, 0xD, 0xE },                     //1-3
+        new byte[] { 0x6A, 0x6B, 0x69, 0x68},                               //7-1
+        new byte[] { 0x5F, 0x60, 0x5F, 0x60, 0x5F, 0x5F, 0x5F, 0x61},       //7-2
+        new byte[] { 0x62, 0x63, 0x63, 0x62, 0x5F, 0x60, 0x60, 0x64},       //7-3
+        new byte[] { 0x62, 0x63, 0x60, 0x5F, 0x60, 0x61, 0x65, 0x66},       //7-4
         
-        new byte[] { 0x18, 0x1A, 0x1D, 0x1E, 0x18, 0x1D, 0x1F, 0x20 },      //2-1
-        new byte[] { 0x19, 0x18, 0x1A, 0x1B, 0x1C, 0x17, 0x18, 0x16 },      //2-2
-        
-        new byte[] { 0x2C, 0x2D, 0x2C, 0x2E, 0x27, 0x28, 0x27, 0x29 },      //3-1 begin
-        //
+        new byte[] { 0x7B, 0x6D, 0x7C, 0x78, 0x79, 0x7A, 0x77},            //8-1
+        new byte[] { 0x71, 0x72, 0x73, 0x72, 0x74, 0x75, 0x76},            //8-2
+        new byte[] { 0x6C, 0x6D, 0x6E, 0x6F, 0x6D, 0x70},                  //8-3
     };
     return new LevelLayerData(levelLayers[levelNo].Length, 1, levelLayers[levelNo]);
   }
   
   public IList<LevelRec> levelRecsCad2 = new List<LevelRec>() 
   {
-    //level 1
-    new LevelRec(0xE401, 12, 8, 8, 0xDE7A, "1-1"),
-    new LevelRec(0xE426, 15, 8, 8, 0xDE7A, "1-2"),
-    new LevelRec(0xE454, 8 , 8, 8, 0xDE7A, "1-3"),
-   //bonus
-    new LevelRec(0xE471, 1 , 8, 8, 0xDE7A, "1-4 (boss)"),
-    
-    //level 2
-    new LevelRec(0xE475, 10, 8, 8, 0xDEBA, "2-1"),
-    new LevelRec(0xE494, 13, 8, 8, 0xDEBA, "2-2"),
-    new LevelRec(0xE4BC, 1 , 8, 8, 0xDEBA, "2-3 (boss)"),
-    //bonuses
-    //copy of boss 2
-    
-    //level 3
-    new LevelRec(0xE4C0, 28, 8, 8, 0xDEFA, "3-1"),
-    new LevelRec(0xE515, 1 , 8, 8, 0xDEFA, "3-2 (boss)"),
-    //bonuses
-    //copy of boss 3
-    
-    //level 4
-    new LevelRec(0xE5BB, 25, 8, 8, 0xDFBA, "4-1"),
-    
-    //level 5
-    new LevelRec(0xE55B, 14, 8, 8, 0xDF7A, "5-1"),
-    new LevelRec(0xE588, 15, 8, 8, 0xDF7A, "5-2"),
-    new LevelRec(0xE5B7, 1 , 8, 8, 0xDF7A, "5-3 (boss)"),
-    //bonuses
-    //copy of boss 5
-    //bonuses
-    //copy of boss 5
-    
-    //level 6
-    new LevelRec(0xE519, 21, 8, 8, 0xDF3A, "6-1"),
-    new LevelRec(0xE559, 1 , 8, 8, 0xDF3A, "6-2 (boss)"),
-    
     //level 7
     new LevelRec(0xE607, 8 , 8, 8, 0xDFFA, "7-1"),
     new LevelRec(0xE620, 7 , 8, 8, 0xDFFA, "7-2"),
@@ -109,14 +77,14 @@ public class Data : CapcomBase
     new LevelRec(0xE747, 1 , 8, 8, 0xE07A, "9-6 (boss)"),
     
     //prizes
-    new LevelRec(0xE75C, 53, 8, 8, 0xDE7A, "1-1 stars"),
-    new LevelRec(0xE7C7, 55, 8, 8, 0xDE7A, "1-2 stars"),
-    new LevelRec(0xE836, 33, 8, 8, 0xDE7A, "1-3 stars"),
+    new LevelRec(0xECC8, 40, 8, 8, 0xDFFA, "7-1 stars"),
+    new LevelRec(0xED19, 20, 8, 8, 0xDFFA, "7-2 stars"),
+    new LevelRec(0xED42, 27, 8, 8, 0xDFFA, "7-3 stars"),
+    new LevelRec(0xED79, 31, 8, 8, 0xDFFA, "7-4 stars"),
     
-    new LevelRec(0xE879, 46, 8, 8, 0xDEBA, "2-1 stars"),
-    new LevelRec(0xE8D6, 57, 8, 8, 0xDEBA, "2-2 stars"),
-    
-    new LevelRec(0xE949, 76, 8, 8, 0xDEFA, "3-1* stars"),
+    new LevelRec(0xEDB8, 57, 8, 8, 0xE03A, "8-1 stars"),
+    new LevelRec(0xEE2B, 23, 8, 8, 0xE03A, "8-2 stars"),
+    new LevelRec(0xEE5A, 32, 8, 8, 0xE03A, "8-3 stars"),
   };
   
   public bool isBigBlockEditorEnabled() { return false; }
