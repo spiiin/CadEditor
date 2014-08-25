22331df9f95db975952382c19a9714ad51fe6d20
a.boyko <a.boyko@gaijin.ru>
2014-08-25 19:34:22 +0400

    settings for Alladin, level 1

---
+++ CadEditor/settings_sega_alladin/Settings_Alladin-1.cs
@@ -0,0 +1,59 @@
+using CadEditor;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Windows.Forms;
+
+public class Data
+{
+  public GameType getGameType()              { return GameType.Generic; }
+  public bool isUseSegaGraphics()            { return true; }
+  //public bool isBuildScreenFromSmallBlocks() { return true; }
+  public int getWordLen()              { return 2;}
+  
+  public OffsetRec getPalOffset()       { return new OffsetRec(0x129052, 1, 64    );   }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0, 1, 0     );   }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0, 1, 0     );   }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x145B2C, 1, 0x8000);  }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0, 1, 0     );   }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0, 1, 155*88);   }
+  public int getBigBlocksCount()        { return 0x1000; }
+  public int getScreenWidth()           { return 155; }
+  public int getScreenHeight()          { return 88; }
+  public IList<LevelRec> getLevelRecs() { return null; }
+  
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChuck;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return null;}
+  public GetPalFunc           getPalFunc()           { return Utils.getPalleteLinear;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  public GetObjectsFunc getObjectsFunc() { return null; }
+  public SetObjectsFunc setObjectsFunc() { return null; }
+  public ConvertScreenTileFunc getConvertScreenTileFunc() { return (v=>v/8);}
+  
+  public byte[] getVideoChuck(int videoPageId)
+  {
+    try
+    {
+        using (FileStream f = File.OpenRead("videoBack_1.bin"))
+        {
+            byte[] d = new byte[0x10000];
+            f.Read(d, 0, 0x10000);
+            return d;
+        }
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message);
+    }
+    return null;
+  }
+  
+  public bool isBigBlockEditorEnabled() { return false;  }
+  public bool isBlockEditorEnabled()    { return false;  }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+  //--------------------------------------------------------------------------------------------
+}
