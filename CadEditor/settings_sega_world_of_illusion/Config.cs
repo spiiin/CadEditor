3dcdb745b19551bd897cf7cb3aa9b0ed4c5dad00
a.boyko <a.boyko@gaijin.ru>
2014-09-23 13:29:46 +0400

    world of illusion, level 1

---
+++ CadEditor/settings_sega_world_of_illusion/Config.cs
@@ -0,0 +1,11 @@
+using CadEditor;
+using System.Collections.Generic;
+using System.Drawing;
+
+public class Config
+{
+  public string getFileName()      {  return "settings_sega_world_of_illusion/dump.bin";         }
+  public string getDumpName()      { return "";                                                  }
+  public string getConfigName()    { return "settings_sega_world_of_illusion/Settings_WIL-1.cs"; }
+  public bool showDumpFileField()  { return false;  }
+}
