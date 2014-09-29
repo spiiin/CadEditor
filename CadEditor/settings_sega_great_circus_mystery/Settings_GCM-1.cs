0f6c690ced1d348bd2224cbdf82f9963ac966c11
Sanya Boyko <sanya.boyko@gmail.com>
2014-09-29 20:28:08 +0400

    great circus mystery settings, level 1

---
+++ CadEditor/settings_sega_great_circus_mystery/Settings_GCM-1.cs
@@ -0,0 +1,78 @@
+using CadEditor;
+using System.Collections.Generic;
+using System.IO;
+using System;
+using System.Windows.Forms;
+
+public class Data
+{
+  public GameType getGameType()              { return GameType.Generic; }
+  public bool isUseSegaGraphics()            { return true; }
+  //public bool isBuildScreenFromSmallBlocks() { return true; }
+  public int getWordLen()              { return 2;}
+  
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 1, 64    );   }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0, 2, 0     );   }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0, 2, 0     );   }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x137000, 1, 0x500);  }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0, 1, 0     );   }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0, 20, 16*16);   }
+  public int getBigBlocksCount()        { return 0x500; }
+  public int getScreenWidth()           { return 16; }
+  public int getScreenHeight()          { return 16; }
+  public IList<LevelRec> getLevelRecs() { return null; }
+  
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChuck;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return Utils.setBigBlocksCapcomDefault;}
+  public GetPalFunc           getPalFunc()           { return readPal;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  public GetObjectsFunc getObjectsFunc() { return null; }
+  public SetObjectsFunc setObjectsFunc() { return null; }
+  public ConvertScreenTileFunc getConvertScreenTileFunc() { return (v=>v/8);}
+  public ConvertScreenTileFunc getBackConvertScreenTileFunc() { return (v=>v*8);}
+  
+  public byte[] getVideoChuck(int videoPageId)
+  {
+    try
+    {
+        using (FileStream f = File.OpenRead(videoPageId == 0x90 ? "vram.bin":"vram2.bin"))
+        {
+            byte[] d = new byte[0x10000];
+            f.Read(d, 0, 0x10000);
+            return d;
+        }
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message);
+    }
+    return null;
+  }
+  
+  public byte[] readPal(int palNo)
+  {
+    try
+    {
+        using (FileStream f = File.OpenRead("pal.bin"))
+        {
+            byte[] d = new byte[128];
+            f.Read(d, 0, 128);
+            return d;
+        }
+    }
+    catch (Exception ex)
+    {
+        MessageBox.Show(ex.Message);
+    }
+    return null;
+  }
+  
+  public bool isBigBlockEditorEnabled() { return false;  }
+  public bool isBlockEditorEnabled()    { return true;  }
+  public bool isLayoutEditorEnabled()   { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isVideoEditorEnabled()    { return false; }
+  //--------------------------------------------------------------------------------------------
+}
