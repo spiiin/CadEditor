d2ccd2384dccf076ec0a95037ec04ef2c2743932
a.boyko <a.boyko@gaijin.ru>
2014-12-23 06:52:15 -0800

    move selectDoorIndex to plugin

---
+++ PluginLevelParamsCad/SelectDoorIndex.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace CadEditor
+{
+    partial class SelectDoorIndex
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SelectDoorIndex));
+            this.cbLevel = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnScreens = new System.Windows.Forms.Panel();
+            this.btOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cbLevel
+            // 
+            this.cbLevel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbLevel.FormattingEnabled = true;
+            this.cbLevel.Items.AddRange(new object[] {
+            "Level 0",
+            "Level A",
+            "Level B",
+            "Level C",
+            "Level D",
+            "Level E",
+            "Level F",
+            "Level G",
+            "Level H",
+            "Level I",
+            "Level J"});
+            this.cbLevel.Location = new System.Drawing.Point(83, 230);
+            this.cbLevel.Name = "cbLevel";
+            this.cbLevel.Size = new System.Drawing.Size(171, 21);
+            this.cbLevel.TabIndex = 0;
+            this.cbLevel.SelectedIndexChanged += new System.EventHandler(this.cbLevel_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(2, 233);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select level:";
+            // 
+            // pnScreens
+            // 
+            this.pnScreens.Location = new System.Drawing.Point(5, 12);
+            this.pnScreens.Name = "pnScreens";
+            this.pnScreens.Size = new System.Drawing.Size(249, 212);
+            this.pnScreens.TabIndex = 2;
+            // 
+            // btOk
+            // 
+            this.btOk.Location = new System.Drawing.Point(5, 257);
+            this.btOk.Name = "btOk";
+            this.btOk.Size = new System.Drawing.Size(249, 23);
+            this.btOk.TabIndex = 3;
+            this.btOk.Text = "OK";
+            this.btOk.UseVisualStyleBackColor = true;
+            this.btOk.Click += new System.EventHandler(this.btOk_Click);
+            // 
+            // SelectDoorIndex
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(257, 280);
+            this.Controls.Add(this.btOk);
+            this.Controls.Add(this.pnScreens);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbLevel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "SelectDoorIndex";
+            this.Text = "SelectDoorIndex";
+            this.Load += new System.EventHandler(this.SelectDoorIndex_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbLevel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnScreens;
+        private System.Windows.Forms.Button btOk;
+    }
+}
