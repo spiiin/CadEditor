0a2b660a6cfdd27631fa97122d2d64fcc128167e
a.boyko <a.boyko@gaijin.ru>
2015-06-25 15:17:52 +0300

    megaman 5 all levels

---
+++ CadEditor/settings_megaman_5/Settings_Megaman5-4.cs
@@ -0,0 +1,33 @@
+using CadEditor;
+using System.Collections.Generic;
+//css_include Settings_CapcomBase.cs;
+public class Data:CapcomBase
+{
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginChrView.dll"
+    };
+  }
+  
+  private int LEVEL_OFFSET = 0x2000;
+  private int CHR_OFFSET   = 0x1000;
+  private int LEVEL_NO     = 3;
+  public OffsetRec getPalOffset()       { return new OffsetRec(0x0998 + LEVEL_OFFSET* LEVEL_NO, 32  , 16);    }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0x60010+ CHR_OFFSET  * LEVEL_NO, 16 , 0x1000); }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0x60010+ CHR_OFFSET  * LEVEL_NO, 16 , 0x1000); } //
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x1210 + LEVEL_OFFSET * LEVEL_NO, 1 , 0x2000); }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x0D10 + LEVEL_OFFSET * LEVEL_NO, 1 , 0x2000); }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0x1610 + LEVEL_OFFSET * LEVEL_NO, 256 , 0x40); }
+  public IList<LevelRec> getLevelRecs() { return levelRecsDwd; } //
+  
+  public IList<LevelRec> levelRecsDwd = new List<LevelRec>() 
+  {
+    new LevelRec(0x10000, 1, 8, 4,  0x41510), 
+  };
+  
+  public bool isBigBlockEditorEnabled() { return true;  }
+  public bool isBlockEditorEnabled()    { return true;  }
+  public bool isEnemyEditorEnabled()    { return false; }
+}
