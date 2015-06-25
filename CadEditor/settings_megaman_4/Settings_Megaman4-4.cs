86af7629bd39612eafb4d2838aa67639895c8e6b
a.boyko <a.boyko@gaijin.ru>
2015-06-25 15:17:15 +0300

    megaman 4 all levels

--- CadEditor/settings_megaman_4/Settings_Megaman4-4.cs
+++ CadEditor/settings_megaman_4/Settings_Megaman4-4.cs
@@ -10,17 +10,20 @@ public class Data:CapcomBase
       "PluginChrView.dll"
     };
   }
-  public OffsetRec getPalOffset()       { return new OffsetRec(0x475A0, 1  , 16);     }
-  public OffsetRec getVideoOffset()     { return new OffsetRec(0x23010, 1   , 0x1000); }
-  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0x20010, 1   , 0x1000); } //
-  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x46510 , 1  , 0x4000); }
-  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x46010 , 1  , 0x4000); }
-  public OffsetRec getScreensOffset()   { return new OffsetRec(0x46910 , 32 , 0x40);   }
+  private static int LEVEL_OFFSET = 0x2000;
+  private static int CHR_OFFSET   = 0x1000;
+  private static int LEVEL_NO     = 3;
+  public OffsetRec getPalOffset()       { return new OffsetRec(0x415A0 + LEVEL_OFFSET* LEVEL_NO, 1  , 16);     }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0x20010 + CHR_OFFSET* LEVEL_NO, 1   , 0x1000); }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0x20010 + CHR_OFFSET* LEVEL_NO, 1   , 0x1000); } //
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x40510 + LEVEL_OFFSET* LEVEL_NO , 1  , 0x4000); }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x40010 + LEVEL_OFFSET* LEVEL_NO , 1  , 0x4000); }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0x40910 + LEVEL_OFFSET* LEVEL_NO, 32 , 0x40);   }
   public IList<LevelRec> getLevelRecs() { return levelRecsDwd; } //
   
   public IList<LevelRec> levelRecsDwd = new List<LevelRec>() 
   {
-    new LevelRec(0x10000, 1, 8, 4,  0x47510), 
+    new LevelRec(0x10000, 1, 8, 4,  0x41510 + LEVEL_OFFSET* LEVEL_NO ), 
   };
   public bool isBigBlockEditorEnabled() { return true;  }
   public bool isBlockEditorEnabled()    { return true;  }
