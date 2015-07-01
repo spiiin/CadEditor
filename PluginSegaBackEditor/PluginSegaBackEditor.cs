1633b03915b5e9201d650685c8d0c874fbda83ff
spiiin <sanya.boyko@gmail.com>
2015-07-02 01:48:18 +0300

    plugin sega back editor

---
+++ PluginSegaBackEditor/PluginSegaBackEditor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Resources;
+using CadEditor;
+
+namespace PluginSegaBackEditor
+{
+    public class PluginSegaBackEditor : IPlugin
+    {
+        public string getName()
+        {
+            return "Sega Back Editor";
+        }
+        public void addSubeditorButton(FormMain formMain)
+        {
+            this.formMain = formMain;
+            var rm = new ResourceManager("PluginSegaBackEditor.Icon", this.GetType().Assembly);
+            var iconMap = (System.Drawing.Bitmap)rm.GetObject("icon_map");
+            var item = new ToolStripButton("Back Editor", iconMap, btMap_Click);
+            item.DisplayStyle = ToolStripItemDisplayStyle.Image;
+            formMain.addSubeditorButton(item);
+        }
+
+        public void addToolButton(FormMain formMain)
+        {
+        }
+
+        private void btMap_Click(object sender, EventArgs e)
+        {
+            var f = new SegaBlockEdit();
+            f.changeModeToBackEdit();
+            formMain.subeditorOpen(f, (ToolStripButton)sender);
+        }
+
+        public void loadFromConfig(object asm, object data)
+        {
+        }
+
+        FormMain formMain;
+    }
+}
