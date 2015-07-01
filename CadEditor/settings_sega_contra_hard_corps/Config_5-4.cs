3d3ca9fb2dc40402446545b6c503d6c81ed497df
a.boyko <a.boyko@gaijin.ru>
2015-07-01 17:20:22 +0300

    contra hard corps, settings for all levels

---
+++ CadEditor/settings_sega_contra_hard_corps/Config_5-4.cs
@@ -0,0 +1,11 @@
+using CadEditor;
+using System.Collections.Generic;
+using System.Drawing;
+
+public class Config
+{
+  public string getFileName()      {  return "Contra - Hard Corps (U) [!].gen";                     }
+  public string getDumpName()      { return "settings_sega_contra_hard_corps/map_54.bin";     }
+  public string getConfigName()    { return "settings_sega_contra_hard_corps/Settings_SegaContra_5-4_layerA.cs"; }
+  public bool showDumpFileField()  { return true;  }
+}
