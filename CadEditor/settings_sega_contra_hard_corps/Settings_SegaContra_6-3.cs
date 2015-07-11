3ae2e6f7f8fb79cb1bdef715bbb2ec9e6f15669a
spiiin <sanya.boyko@gmail.com>
2015-07-11 19:05:49 +0300

    chcutils

--- CadEditor/settings_sega_contra_hard_corps/Settings_SegaContra_6-3.cs
+++ CadEditor/settings_sega_contra_hard_corps/Settings_SegaContra_6-3.cs
@@ -1,6 +1,7 @@
 using CadEditor;
 using System;
 using System.Collections.Generic;
+//css_include Settings_CHC-Utils.cs;
 
 public class Data 
 { 
@@ -33,7 +34,7 @@ public class Data
   public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return true; }
   
-  public GetObjectsFunc getObjectsFunc() { return getObjects; }
+  public GetObjectsFunc getObjectsFunc() { return CHCUtils.getObjects; }
   public SetObjectsFunc setObjectsFunc() { return null; }
   public IList<LevelRec> getLevelRecs()  { return levelRecs;  }
   public GetLayoutFunc getLayoutFunc()   { return getLayout;  }
@@ -49,72 +50,6 @@ public class Data
   private string BLOCKS_NAME = "blocks_63.bin"; //1EC630//1EEAF4
   private string PAL_NAME    = "pal_63.bin";
   private string BACK_NAME    = "back_63.bin"; //1ED2CC
-  
-  public List<ObjectRec> getObjects(int levelNo)
-  {
-      int OBJ_SIZE = 14;
-      var commandDict = new Dictionary<int,int> {
-        { 0xFFFF , 1},
-        { 0xFFFE , 5},
-        { 0xFFFD , 8},
-        { 0xFFFC , 8},
-        { 0xFFFB , 8},
-        { 0xFFFA , 5},
-        { 0xFFF9 , 11},
-        { 0xFFF8 , 4},
-        { 0xFFF7 , 7},
-        { 0xFFF6 , 1},
-        { 0xFFF5 , 1},
-        { 0xFFF4 , 2},
-        { 0xFFF3 , 2},
-        { 0xFFF2 , 1},
-        { 0xFFF1 , 1},
-        { 0xFFF0 , 4},
-        { 0xFFEF , 6},
-        { 0xFFEE , 6},
-        { 0xFFED , 6},
-      };
-      
-      LevelRec lr = ConfigScript.getLevelRec(levelNo);
-      int objCount = lr.objCount, addr = lr.objectsBeginAddr;
-      var objects = new List<ObjectRec>();
-      int i = 0;
-      int curAddr = addr;
-      while (objects.Count != objCount)
-      {
-          int baseAddr = curAddr;
-          int v = Utils.readWord(Globals.romdata, baseAddr);
-          if (v < 0xFF00)
-          {
-            int sx, sy, x, y, baseX, baseY, fromFloor;
-            sx = 0;
-            sy = 0;
-            x = Utils.readWord(Globals.romdata, baseAddr + 8);
-            y = Utils.readWord(Globals.romdata, baseAddr + 10);
-            fromFloor = Utils.readWord(Globals.romdata, baseAddr + 6);
-            if (fromFloor == 0)
-            {
-              x = Utils.readWord(Globals.romdata, baseAddr + 2);
-              x += Utils.readWord(Globals.romdata, baseAddr + 8)/2;
-              y = Utils.readWord(Globals.romdata, baseAddr + 4);
-              y += Utils.readWord(Globals.romdata, baseAddr + 10)/2;
-            }
-            var obj = new ObjectRec(v, sx, sy, x, y);
-            objects.Add(obj);
-            curAddr += OBJ_SIZE;
-          }
-          else
-          {
-            curAddr += commandDict[v]*2;
-          }
-      }
-      return objects;
-  }
-    
-  public bool setObjects(int levelNo, List<ObjectRec> objects)
-  {
-      return true;
-  }
   
   LevelLayerData getLayout(int levelNo)
   {
