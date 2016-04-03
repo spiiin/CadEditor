d07ea209ac62fa9f07b70303346c98d55210808c
spiiin <sanya.boyko@gmail.com>
2016-04-04 02:10:39 +0300

    fix

--- CadEditor/settings_flintstones_rescue_of_dino_and_hoppy/Settings_Flintstones-3-rom.cs
+++ CadEditor/settings_flintstones_rescue_of_dino_and_hoppy/Settings_Flintstones-3-rom.cs
@@ -10,7 +10,7 @@ public class Data
       "PluginChrView.dll",
     };
   }
-  public OffsetRec getScreensOffset()   { return new OffsetRec(0x122BF, 1 , 8*96);   }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0x122BF, 1 , 8*64);   }
   public OffsetRec getPalOffset()       { return new OffsetRec(0x0    , 1  , 16);     }
   public OffsetRec getVideoOffset()     { return new OffsetRec(0x3E010, 1  , 0x1000); }
   public OffsetRec getVideoObjOffset()  { return new OffsetRec(0x20010, 1  , 0x1000); }
@@ -19,7 +19,7 @@ public class Data
   public OffsetRec getBlocksOffset()    { return new OffsetRec(0x12ABF, 1  , 0x4000); }
   public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x125BF, 1  , 0x4000); }
   public int getScreenWidth()             { return 8; }
-  public int getScreenHeight()            { return 96; }
+  public int getScreenHeight()            { return 64; }
   public bool getScreenVertical()         { return true; }
   //public string getBlocksFilename()       { return "flintstones_3.png"; }
   
