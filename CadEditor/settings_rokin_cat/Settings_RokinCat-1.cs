a2097bd883a2f49656033a708d7bcfce4f3c3ef4
spiiin <sanya.boyko@gmail.com>
2016-11-22 21:40:22 +0200

    remove picture version

--- CadEditor/settings_rokin_cat/Settings_RokinCat-1.cs
+++ CadEditor/settings_rokin_cat/Settings_RokinCat-1.cs
@@ -7,8 +7,6 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec(0x13779   , 19 , 3*2);   }
   public int getScreenWidth()          { return 2; }
   public int getScreenHeight()         { return 3; }
-  public string getBlocksFilename()    { return "rokin_cat_1.png"; }
-  public int    getPictureBlocksWidth(){ return 64; }
   
   public bool isBigBlockEditorEnabled() { return true; }
   public bool isBlockEditorEnabled()    { return true; }
