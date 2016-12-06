a4e949917dc8ea7e8d3113aaa9a0c7b4d85e6a5f
spiiin <sanya.boyko@gmail.com>
2016-12-07 00:50:29 +0200

    settings for rockin' kats levels 2-1 - 2-6

---
+++ CadEditor/settings_rockin_cats/Settings_RoсkinCats-2-1.cs
@@ -0,0 +1,38 @@
+using CadEditor;
+using System;
+using System.Drawing;
+//css_include Settings_RockinCats-Utils;
+
+public class Data : RockinCatsBase
+{
+  public override OffsetRec getScreensOffset()  { return new OffsetRec(0x15499, 10 , 3*2);    }
+  public override int getVideoIndex1()          { return 0x22; }
+  public override int getVideoIndex2()          { return 0x24; }
+  public override OffsetRec getBlocksOffset()   { return new OffsetRec(0x15142 ,1  , 0x4000); }
+  public override int getBlocksCount()          { return 79; }
+  
+  public override OffsetRec getBigBlocksOffsetHierarchy(int hierarchyLevel)
+  { 
+    if (hierarchyLevel == 0) { return new OffsetRec(0x152cd, 1  , 0x4000); }
+    if (hierarchyLevel == 1) { return new OffsetRec(0x153d5, 1  , 0x4000); }
+    if (hierarchyLevel == 2) { return new OffsetRec(0x15469, 1  , 0x4000); }
+    return new OffsetRec(0x0, 1  , 0x4000);
+  }
+  
+  public override int getBigBlocksCountHierarchy(int hierarchyLevel)
+  { 
+    if (hierarchyLevel == 0) { return 66; }
+    if (hierarchyLevel == 1) { return 37; }
+    if (hierarchyLevel == 2) { return 24;  }
+    return 256;
+  }
+  
+  public override byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x16, 0x28, 0x30, 0x0f, 0x02, 0x10, 0x00,
+      0x0f, 0x02, 0x0a, 0x1a, 0x0f, 0x02, 0x12, 0x31,
+    }; 
+    return pallete;
+  }
+}
---
+++ CadEditor/settings_rockin_cats/Settings_RoсkinCats-2-3and456.cs
@@ -0,0 +1,38 @@
+using CadEditor;
+using System;
+using System.Drawing;
+//css_include Settings_RockinCats-Utils;
+
+public class Data : RockinCatsBase
+{
+  public override OffsetRec getScreensOffset()  { return new OffsetRec(0x15cc1, 23 , 3*2);    }
+  public override int getVideoIndex1()          { return 0x28; }
+  public override int getVideoIndex2()          { return 0x2a; }
+  public override OffsetRec getBlocksOffset()   { return new OffsetRec(0x15812 ,1  , 0x4000); }
+  public override int getBlocksCount()          { return 77; }
+  
+  public override OffsetRec getBigBlocksOffsetHierarchy(int hierarchyLevel)
+  { 
+    if (hierarchyLevel == 0) { return new OffsetRec(0x15993, 1  , 0x4000); }
+    if (hierarchyLevel == 1) { return new OffsetRec(0x15b37, 1  , 0x4000); }
+    if (hierarchyLevel == 2) { return new OffsetRec(0x15c57, 1  , 0x4000); }
+    return new OffsetRec(0x0, 1  , 0x4000);
+  }
+  
+  public override int getBigBlocksCountHierarchy(int hierarchyLevel)
+  { 
+    if (hierarchyLevel == 0) { return 105; }
+    if (hierarchyLevel == 1) { return 72; }
+    if (hierarchyLevel == 2) { return 53;  }
+    return 256;
+  }
+  
+  public override byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x16, 0x28, 0x30, 0x0f, 0x0c, 0x1c, 0x2c,
+      0x0f, 0x0c, 0x00, 0x10, 0x0f, 0x16, 0x27, 0x37,
+    }; 
+    return pallete;
+  }
+}
