af4816bd3b7490857cc26aa6275b396ef4811164
spiiin <sanya.boyko@gmail.com>
2016-12-10 16:10:06 +0200

    change blocks pal in types encoding

--- CadEditor/settings_chip_and_dale_2/Settings_ChipAndDale2-All.cs
+++ CadEditor/settings_chip_and_dale_2/Settings_ChipAndDale2-All.cs
@@ -24,6 +24,8 @@ public class Data : CapcomBase
   public int getScrollsOffsetFromLayout() { return 538; } //offset scrolls array from layout array
   public GetObjectsFunc getObjectsFunc()   { return getObjectsCad2; }
   public SetObjectsFunc setObjectsFunc()   { return setObjectsCad2; }
+  public override GetBlocksFunc        getBlocksFunc()        { return getBlocksDt2;}
+  public override SetBlocksFunc        setBlocksFunc()        { return setBlocksDt2;}
   public override GetVideoPageAddrFunc getVideoPageAddrFunc() { return getChrAddress; }
   public bool isBuildScreenFromSmallBlocks() { return true; }
   public GetLayoutFunc getLayoutFunc()     { return getLayout;   }
@@ -229,5 +231,74 @@ public class Data : CapcomBase
       if (id >= 0x90)
         return ConfigScript.videoOffset.beginAddr + ConfigScript.videoOffset.recSize * (id - 0x90);
       return -1;
+  }
+  
+  //-----------------------------------------------------------------------------------------------
+  public class Dt2ObjRec : ObjRec
+  {
+    public Dt2ObjRec(ObjRec b, int _index)
+        :base(b)
+    {
+        index = _index;
+    }
+    
+    public override int getType()
+    {
+        return index < 0xA0 ? 0 : 5;
+    }
+    
+    int index;
+  }
+  
+  public ObjRec[] getBlocksDt2(int blockIndex)
+  {
+    ObjRec[] blocks = Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(blockIndex), getBlocksCount());
+    //decode typeColor
+    int palInfoCount = getBlocksCount()/4;
+    var palInfo = new byte[palInfoCount];
+    for (int i = 0; i < palInfoCount; i++)
+    {
+        palInfo[i] = blocks[i].typeColor;
+    }
+    for (int i = 0; i < blocks.Length; i++)
+    {
+        var palInfoByte = palInfo[i/4];
+        int parByteNo = i % 4;
+        blocks[i].typeColor = (byte)((palInfoByte >> parByteNo*2) & 3);
+    }
+    //
+    //rebuild blocks to dt2 blocks
+    for (int i = 0; i < blocks.Length; i++)
+    {
+        blocks[i] = new Dt2ObjRec(blocks[i], i);
+    }
+    //
+    return blocks;
+  }
+  
+  public void setBlocksDt2(int blockIndex, ObjRec[] objects)
+  {
+    int addr = ConfigScript.getTilesAddr(blockIndex);
+    int count = getBlocksCount();
+    for (int i = 0; i < count; i++)
+    {
+        var obj = objects[i];
+        Globals.romdata[addr + i] = obj.c1;
+        Globals.romdata[addr + count * 1 + i] = obj.c2;
+        Globals.romdata[addr + count * 2 + i] = obj.c3;
+        Globals.romdata[addr + count * 3 + i] = obj.c4;
+    }
+    
+    int palInfoCount = getBlocksCount()/4;
+    for (int i = 0; i < palInfoCount; i++)
+    {
+        var palInfoByte = 
+          (objects[i*4+0].typeColor<<0) | 
+          (objects[i*4+1].typeColor<<2) |
+          (objects[i*4+2].typeColor<<4) | 
+          (objects[i*4+3].typeColor<<6);
+          
+        Globals.romdata[addr + count * 4 + i] = (byte)palInfoByte;
+    }
   }
 }
