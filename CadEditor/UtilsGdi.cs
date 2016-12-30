656f6af37da907c3eb2123876ab4be4d28da6ebd
spiiin <sanya.boyko@gmail.com>
2016-12-30 18:58:33 +0200

    check bitmaps eq

--- CadEditor/UtilsGdi.cs
+++ CadEditor/UtilsGdi.cs
@@ -8,6 +8,7 @@ using System.Collections.Generic;
 using System.Drawing.Drawing2D;
 using ResampleTest;
 using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
 
 namespace CadEditor
 {
@@ -68,6 +69,47 @@ namespace CadEditor
                 }
             }
             return totalImage;
+        }
+
+        public static bool CompareBitmaps(Image left, Image right)
+        {
+            if (object.Equals(left, right))
+                return true;
+            if (left == null || right == null)
+                return false;
+            if (!left.Size.Equals(right.Size) || !left.PixelFormat.Equals(right.PixelFormat))
+                return false;
+
+            Bitmap leftBitmap = left as Bitmap;
+            Bitmap rightBitmap = right as Bitmap;
+            if (leftBitmap == null || rightBitmap == null)
+                return true;
+
+            int bytes = left.Width * left.Height * (Image.GetPixelFormatSize(left.PixelFormat) / 8);
+
+            bool result = true;
+            byte[] b1bytes = new byte[bytes];
+            byte[] b2bytes = new byte[bytes];
+
+            BitmapData bmd1 = leftBitmap.LockBits(new Rectangle(0, 0, leftBitmap.Width - 1, leftBitmap.Height - 1), ImageLockMode.ReadOnly, leftBitmap.PixelFormat);
+            BitmapData bmd2 = rightBitmap.LockBits(new Rectangle(0, 0, rightBitmap.Width - 1, rightBitmap.Height - 1), ImageLockMode.ReadOnly, rightBitmap.PixelFormat);
+
+            Marshal.Copy(bmd1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bmd2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n <= bytes - 1; n++)
+            {
+                if (b1bytes[n] != b2bytes[n])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            leftBitmap.UnlockBits(bmd1);
+            rightBitmap.UnlockBits(bmd2);
+
+            return result;
         }
 
         private static ushort[][,] ConvertBitmapToArray(Bitmap bmp)
