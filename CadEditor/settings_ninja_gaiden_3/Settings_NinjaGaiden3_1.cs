eb560be487ca1378dea05663e7e1db398bb240da
spiiin <sanya.boyko@gmail.com>
2017-01-04 12:52:41 +0200

    build blocks and bigblocks from ppu dump (big blocks with pal bytes as in tiny toon)

--- CadEditor/settings_ninja_gaiden_3/Settings_NinjaGaiden3_1.cs
+++ CadEditor/settings_ninja_gaiden_3/Settings_NinjaGaiden3_1.cs
@@ -4,13 +4,140 @@ using System.Drawing;
 
 public class Data 
 { 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginChrView.dll",
+    };
+  }
+  
   public OffsetRec getScreensOffset()  { return new OffsetRec(0x10, 1 , 6*40);   }
   public int getScreenWidth()          { return 6; }
   public int getScreenHeight()         { return 40; }
-  public string getBlocksFilename()    { return "ninja_gaiden3_1.png"; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x4010 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  
+  public OffsetRec getBigBlocksOffset()    { return new OffsetRec(0x5C10 , 1  , 0x1000);  }
+  public int getBigBlocksCount()           { return 96; }
+  
+  public int getPalBytesAddr()             { return 0x7A10; }
+
   public bool getScreenVertical()      { return true; }
   
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  public GetBlocksFunc        getBlocksFunc()        { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc()        { return setBlocks;}
+  
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return getBigBlocksTT;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return setBigBlocksTT;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump1.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x31, 0x30, 0x0f, 0x0f, 0x07, 0x10, 0x30,
+      0x0f, 0x0c, 0x08, 0x18, 0x0f, 0x07, 0x17, 0x27
+    }; 
+    return pallete;
+  }
+  
+  public ObjRec[] getBlocks(int blockIndex)
+  {
+    return readBlocksLinearTT(Globals.romdata, ConfigScript.getTilesAddr(blockIndex), ConfigScript.getBlocksCount());
+  }
+  
+  public void setBlocks(int blockIndex, ObjRec[] blocksData)
+  {
+    writeBlocksLinearTT(blocksData, Globals.romdata, ConfigScript.getTilesAddr(blockIndex), ConfigScript.getBlocksCount());
+  }
+  
+  ObjRec[] readBlocksLinearTT(byte[] romdata, int addr, int count)
+  {
+    var objects = new ObjRec[count];
+    for (int i = 0; i < count; i++)
+    {
+        byte c1, c2, c3, c4, typeColor;
+        c1 = romdata[addr + i * 4 + 0];
+        c2 = romdata[addr + i * 4 + 1];
+        c3 = romdata[addr + i * 4 + 2];
+        c4 = romdata[addr + i * 4 + 3];
+        typeColor = 0;
+        objects[i] = new ObjRec(c1, c2, c3, c4, typeColor);
+    }
+    return objects;
+  }
+  
+  void writeBlocksLinearTT(ObjRec[] objects, byte[] romdata, int addr, int count)
+  {
+    for (int i = 0; i < count; i++)
+    {
+        var obj = objects[i];
+        romdata[addr + i * 4 + 0] = obj.c1;
+        romdata[addr + i * 4 + 1] = obj.c2;
+        romdata[addr + i * 4 + 2] = obj.c3;
+        romdata[addr + i * 4 + 3] = obj.c4;
+    }
+  }
+  
+  //
+  
+  private byte getTTSmallBlocksColorByte(int index)
+  {
+    return Globals.romdata[getPalBytesAddr()+index];
+  }
+  
+  private  void setTTSmallBlocksColorByte(int index, byte colorByte)
+  {
+    Globals.romdata[getPalBytesAddr()+index] = colorByte;
+  }
+  
+  public BigBlock[] getBigBlocksTT(int bigTileIndex)
+  {
+    var data = Utils.readLinearBigBlockData(0, bigTileIndex, 4);
+    var bb = Utils.unlinearizeBigBlocks<BigBlockWithPal>(data, 2, 2);
+    for (int i = 0; i < bb.Length; i++)
+    {
+      int palByte = getTTSmallBlocksColorByte(i);
+      bb[i].palBytes[0] = palByte >> 0 & 0x3;
+      bb[i].palBytes[1] = palByte >> 2 & 0x3;
+      bb[i].palBytes[2] = palByte >> 4 & 0x3;
+      bb[i].palBytes[3] = palByte >> 6 & 0x3;
+    }
+    return bb;
+  }
+  
+  public void setBigBlocksTT(int bigTileIndex, BigBlock[] bigBlockIndexes)
+  {
+    var data = Utils.linearizeBigBlocks(bigBlockIndexes);
+    Utils.writeLinearBigBlockData(0, bigTileIndex, data);
+    //save pal bytes
+    for (int i = 0; i < bigBlockIndexes.Length; i++)
+    {
+      var bb = bigBlockIndexes[i] as BigBlockWithPal;
+      int palByte = bb.palBytes[0] | bb.palBytes[1] << 2 | bb.palBytes[2]<<4 | bb.palBytes[3]<< 6;
+      setTTSmallBlocksColorByte(i, (byte)palByte);
+    }
+  }
 }
