136341559557f1a75b478b2479f2fbb142e9b2bd
spiiin <sanya.boyko@gmail.com>
2017-01-05 17:32:10 +0200

    build blocks from rom for power blade 2 (1 level)

--- CadEditor/settings_power_blade_2/Settings_PowerBlade2-1.cs
+++ CadEditor/settings_power_blade_2/Settings_PowerBlade2-1.cs
@@ -1,20 +1,107 @@
 using CadEditor;
 using System.Collections.Generic;
 //css_include Settings_CapcomBase.cs;
-public class Data : CapcomBase
+public class Data
 { 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginChrView.dll",
+    };
+  }
   public OffsetRec getPalOffset()       { return new OffsetRec(0, 32  , 16);     }
   public OffsetRec getVideoOffset()     { return new OffsetRec(0, 16  , 0x1000); }
-  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0, 16  , 0x1000); }
-  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0 , 8   , 0x4000); }
-  public OffsetRec getBlocksOffset()    { return new OffsetRec(0 , 8   , 0x4000); }
   public OffsetRec getScreensOffset()   { return new OffsetRec(0x4E45  , 32 , 8*5);   }
-  public override int getScreenWidth()    { return 8; }
-  public override int getScreenHeight()   { return 5; }
-  public string getBlocksFilename() { return "power_blade2_1.png"; }
+  public int getScreenWidth()    { return 8; }
+  public int getScreenHeight()   { return 5; }
   public IList<LevelRec> getLevelRecs() { return null; }
   
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x40FC , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 128; }
+  public int getBigBlocksCount()           { return 128; }
+  public int getPalBytesAddr()          { return 0x402b; }
+  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump1.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x27, 0x16, 0x38, 0x0f, 0x17, 0x06, 0x38,
+      0x0f, 0x11, 0x01, 0x23, 0x0f, 0x1a, 0x08, 0x39
+    }; 
+    return pallete;
+  }
+  
+  public ObjRec[] getBlocks(int blockIndex)
+  {
+    return readBlocksLinearTT(Globals.romdata, ConfigScript.getTilesAddr(blockIndex), ConfigScript.getBlocksCount());
+  }
+  
+  public void setBlocks(int blockIndex, ObjRec[] blocksData)
+  {
+    writeBlocksLinearTT(blocksData, Globals.romdata, ConfigScript.getTilesAddr(blockIndex), ConfigScript.getBlocksCount());
+  }
+  
+  ObjRec[] readBlocksLinearTT(byte[] romdata, int addr, int count)
+  {
+    int BLOCK_W = 4;
+    int BLOCK_H = 4;
+    int BLOCK_S = BLOCK_H * BLOCK_H;
+    var objects = new ObjRec[count];
+    for (int i = 0; i < count; i++)
+    {
+      var indexes = new int[BLOCK_S];
+      var palBytes = new int[BLOCK_S/4];
+      for (int bi = 0; bi < BLOCK_S; bi++)
+      {
+        indexes[bi] = romdata[addr + i*BLOCK_S + bi];
+      }
+      int palByte = romdata[getPalBytesAddr()+i];
+      palBytes = new int[] { (palByte>>0)&3, (palByte>>2)&3, (palByte>>4)&3, (palByte>>6)&3 };
+      
+      objects[i] = new ObjRec(BLOCK_W, BLOCK_H, indexes, palBytes);
+    }
+    return objects;
+  }
+  
+  void writeBlocksLinearTT(ObjRec[] objects, byte[] romdata, int addr, int count)
+  {
+    for (int i = 0; i < count; i++)
+    {
+      var obj = objects[i];
+      int BLOCK_S = obj.indexes.Length;
+      for (int bi = 0; bi < BLOCK_S; bi++)
+      {
+         romdata[addr + i*BLOCK_S + bi] = (byte)obj.indexes[bi];
+      }
+      var objPalBytes = obj.palBytes;
+      int palByte = objPalBytes[0] | objPalBytes[1]<<2 | objPalBytes[2]<<4 | objPalBytes[3]<<6;
+      romdata[getPalBytesAddr()+i] = (byte)palByte;
+    }
+  }
 }
