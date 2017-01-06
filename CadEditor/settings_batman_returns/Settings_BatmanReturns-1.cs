c903c184ba8ecf97dde5390c36ab0631d6b6eefb
spiiin <sanya.boyko@gmail.com>
2017-01-06 16:15:37 +0200

    build from rom

--- CadEditor/settings_batman_returns/Settings_BatmanReturns-1.cs
+++ CadEditor/settings_batman_returns/Settings_BatmanReturns-1.cs
@@ -7,10 +7,43 @@ public class Data
   public OffsetRec getScreensOffset() { return new OffsetRec(0x446E, 6, 8*6); }
   public int getScreenWidth()         { return 8; }
   public int getScreenHeight()        { return 6; }
-  public string getBlocksFilename()    { return "batman_returns_1.png"; }
   public int getBigBlocksCount() { return 65; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x4011 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 65; }
+  public int getPalBytesAddr()          { return 0x4421; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump1.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x20, 0x2a, 0x01, 0x0f, 0x10, 0x1c, 0x0b,
+      0x0f, 0x10, 0x00, 0x0b, 0x0f, 0x27, 0x17, 0x06
+    }; 
+    return pallete;
+  }
 }
