a57f366d82f083ed01c63f217b76cad51ac0518b
spiiin <sanya.boyko@gmail.com>
2017-01-12 17:54:23 +0300

    build from rom

--- CadEditor/settings_super_c/Settings_SuperC-1.cs
+++ CadEditor/settings_super_c/Settings_SuperC-1.cs
@@ -7,9 +7,45 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec(0xA068, 17 , 8*8);   }
   public int getScreenWidth()          { return 8; }
   public int getScreenHeight()         { return 8; }
-  public string getBlocksFilename()    { return "superc_1.png"; }
+  //public string getBlocksFilename()    { return "superc_1.png"; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xa4e8, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 244; }
+  public int getBigBlocksCount()           { return 244; }
+  public int getPalBytesAddr()          { return 0xb428; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump1.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x20, 0x10, 0x00, 0x0f, 0x0c, 0x10, 0x00,
+      0x0f, 0x27, 0x16, 0x04, 0x0f, 0x16, 0x06, 0x00
+    }; 
+    return pallete;
+  }
 }
