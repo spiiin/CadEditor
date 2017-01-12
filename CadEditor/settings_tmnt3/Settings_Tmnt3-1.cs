30cc83596caf8f4b65211035bfcf70d8660b1bd9
spiiin <sanya.boyko@gmail.com>
2017-01-12 22:47:03 +0300

    build from rom

--- CadEditor/settings_tmnt3/Settings_Tmnt3-1.cs
+++ CadEditor/settings_tmnt3/Settings_Tmnt3-1.cs
@@ -7,9 +7,44 @@ public class Data
   public OffsetRec getScreensOffset() { return new OffsetRec(0x300B1, 10, 64); }
   public int getScreenWidth()         { return 8; }
   public int getScreenHeight()        { return 8; }
-  public string getBlocksFilename()   { return "tmnt3_1.png"; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x30471 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 220; }
+  public int getBigBlocksCount()        { return 220; }
+  public int getPalBytesAddr()          { return 0x31231; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump1.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x20, 0x2a, 0x26, 0x0f, 0x37, 0x21, 0x28,
+      0x0f, 0x37, 0x21, 0x20, 0x0f, 0x37, 0x1a, 0x18
+    }; 
+    return pallete;
+  }
 }
