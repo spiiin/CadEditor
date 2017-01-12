0c5237edd4f8f5d5634016411254bf01358e0360
spiiin <sanya.boyko@gmail.com>
2017-01-13 02:32:42 +0300

    build from rom

--- CadEditor/settings_darkman/Settings_Darkman-1.cs
+++ CadEditor/settings_darkman/Settings_Darkman-1.cs
@@ -7,9 +7,26 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec(0xaa48, 1 , 40*7);   }
   public int getScreenWidth()          { return 40; }
   public int getScreenHeight()         { return 7; }
-  public string getBlocksFilename()    { return "darkman_1.png"; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getVideoOffset()   { return new OffsetRec(0x21010, 1, 0x1000); }
+  public OffsetRec getPalOffset()     { return new OffsetRec(0x70e9, 16, 16   ); }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()         { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()            { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()            { return Utils.setVideoChunk; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xa338 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0xa238; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  public GetPalFunc           getPalFunc() { return Utils.getPalleteLinear;}
+  public SetPalFunc           setPalFunc() { return Utils.setPalleteLinear;}
 }
