bfca25e7986e6514feebe99616d77be2c2140eb2
spiiin <sanya.boyko@gmail.com>
2017-01-20 03:13:56 +0300

    config for level 2

---
+++ CadEditor/settings_battletoads/Settings_Battletoads-2.cs
@@ -0,0 +1,48 @@
+using CadEditor;
+using System.Collections.Generic;
+public class Data
+{ 
+  public OffsetRec getScreensOffset()     { return new OffsetRec(0x28f8e  , 1 , 8*56);  }
+  public int getScreenWidth()    { return 8; }
+  public int getScreenHeight()   { return 56; }
+  public int getBigBlocksCount() { return 22; }
+  public int getBlocksCount()    { return 22; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x2914e , 1  , 0x1000);  }
+  public int getPalBytesAddr()          { return 0x292ae; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump2.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x07, 0x06, 0x16, 0x0f, 0x06, 0x16, 0x26,
+      0x0f, 0x06, 0x15, 0x25, 0x0f, 0x06, 0x05, 0x20
+    }; 
+    return pallete;
+  }
+}
