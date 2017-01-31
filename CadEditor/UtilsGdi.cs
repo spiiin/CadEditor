18a70d2b9271e4bb2bdc500e7be5997441d87fe5
spiiin <sanya.boyko@gmail.com>
2017-01-31 21:40:29 +0300

    remove resample service (it slow and no need after refactor pluginvideones)

--- CadEditor/UtilsGdi.cs
+++ CadEditor/UtilsGdi.cs
@@ -6,7 +6,6 @@ using System.Drawing;
 using System.Linq;
 using System.Collections.Generic;
 using System.Drawing.Drawing2D;
-using ResampleTest;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 
@@ -23,14 +22,28 @@ namespace CadEditor
             return bmp;
         }
 
-        public static Image ResizeBitmap(Image sourceBMP, int width, int height)
+        public static Image ResizeBitmap(Image image, int width, int height)
         {
-            ResamplingService resamplingService = new ResamplingService();
-            resamplingService.Filter = ResamplingFilters.Box;
-            ushort[][,] input = ConvertBitmapToArray((Bitmap)sourceBMP);
-            ushort[][,] output = resamplingService.Resample(input, width, height);
-            Image imgCustom = (Image)ConvertArrayToBitmap(output);
-            return imgCustom;
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+            return destImage;
         }
 
         //glue image together
