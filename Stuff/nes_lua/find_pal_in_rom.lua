dc5c54ef71ca5329e6924def202ca812a0d7be53
spiiin <sanya.boyko@gmail.com>
2017-02-03 00:32:12 +0300

    lua script for find pal addr in rom

---
+++ Stuff/nes_lua/find_pal_in_rom.lua
@@ -0,0 +1,20 @@
+--Script for find current loaded background pallete in ROM file
+--Rom: any
+--Author: spiiin
+
+ROM_LEN = rom.readbyte(4) * 0x4000 + 0x10
+pal = ppu.readbyterange(0x3F00, 0x16)
+
+for curAddr = 0x10, ROM_LEN-0x10 do
+  gotoNextIter = false
+  for curOffset = 1, 16 do
+    b = rom.readbyte(curAddr + curOffset)
+    if b ~= pal:sub(curOffset,curOffset):byte() then
+      gotoNextIter = true
+      break
+    end
+  end
+  if not gotoNextIter then
+    print ("Pal address:", (string.format("%05X",curAddr)))
+  end
+end
