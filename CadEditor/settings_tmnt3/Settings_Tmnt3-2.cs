354547046001e50be09e2416b1932ba512c7a8d8
spiiin <sanya.boyko@gmail.com>
2017-02-27 02:46:43 +0300

    tmnt 3, level 2 build from rom

--- CadEditor/settings_tmnt3/Settings_Tmnt3-2.cs
+++ CadEditor/settings_tmnt3/Settings_Tmnt3-2.cs
@@ -4,12 +4,38 @@ using System.Drawing;
 
 public class Data 
 { 
-  public OffsetRec getScreensOffset() { return new OffsetRec(0x31BF7, 6, 8*7); }
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x318F7, 6, 8*7); }
+  
   public int getScreenWidth()         { return 8; }
   public int getScreenHeight()        { return 7; }
-  public string getBlocksFilename()   { return "tmnt3_2.png"; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
   public bool isBlockEditorEnabled()    { return false; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  public OffsetRec getVideoOffset()                          { return new OffsetRec(0x6c010, 1  , 0x1000); }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x31a47 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 112; }
+  public int getBigBlocksCount()        { return 112; }
+  public int getPalBytesAddr()          { return 0x31a47+112*16; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 1, 16);  }
+  
+  //----------------------------------------------------------------------------
+  
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal2.bin");
+  }
 }
