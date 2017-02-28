632b61347e1e9bc12ecfe04b59305bd09aa102ac
spiiin <sanya.boyko@gmail.com>
2017-02-28 20:58:41 +0300

    tmnt 3 level 4 build from rom

--- CadEditor/settings_tmnt3/Settings_Tmnt3-4.cs
+++ CadEditor/settings_tmnt3/Settings_Tmnt3-4.cs
@@ -7,9 +7,33 @@ public class Data
   public OffsetRec getScreensOffset() { return new OffsetRec(0x29399, 19, 64); }
   public int getScreenWidth()         { return 8; }
   public int getScreenHeight()        { return 8; }
-  public string getBlocksFilename()   { return "tmnt3_4.png"; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  public OffsetRec getVideoOffset()                          { return new OffsetRec(0x67010, 1  , 0x1000); }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x29871 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 255; }
+  public int getBigBlocksCount()        { return 255; }
+  public int getPalBytesAddr()          { return 0x29871+255*16; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 1, 16);  }
+  
+  //----------------------------------------------------------------------------
+  
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal4.bin");
+  }
 }
