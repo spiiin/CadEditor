f7f4ab12d2d0a9f1decc0b80534c1aeab235df75
spiiin <sanya.boyko@gmail.com>
2017-02-28 22:25:47 +0300

    tmnt 3 level 8 build from rom

--- CadEditor/settings_tmnt3/Settings_Tmnt3-8-2.cs
+++ CadEditor/settings_tmnt3/Settings_Tmnt3-8-2.cs
@@ -7,9 +7,33 @@ public class Data
   public OffsetRec getScreensOffset() { return new OffsetRec(0x2804D + 48*3, 2, 8*7); }
   public int getScreenWidth()         { return 8; }
   public int getScreenHeight()        { return 7; }
-  public string getBlocksFilename()   { return "tmnt3_8.png"; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  public OffsetRec getVideoOffset()                          { return new OffsetRec(0x70010, 1  , 0x1000); }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x282dd , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 219; }
+  public int getBigBlocksCount()        { return 219; }
+  public int getPalBytesAddr()          { return 0x282dd+219*16; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 1, 16);  }
+  
+  //----------------------------------------------------------------------------
+  
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal8.bin");
+  }
 }
