a8df0984fcd7ac18ddcd005b6de6310318eed542
spiiin <sanya.boyko@gmail.com>
2017-03-01 03:11:00 +0300

    tmnt 2, levels 6-7

---
+++ CadEditor/settings_tmnt2/Settings_Tmnt2-7.cs
@@ -0,0 +1,39 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0xFB72, 10, 8*6); }
+  public int getScreenWidth()         { return 8; }
+  public int getScreenHeight()        { return 6; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  public OffsetRec getVideoOffset()                          { return new OffsetRec(/*0x41010*/ 0x5F010, 1  , 0x1000); }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xEF82 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 167; }
+  public int getBigBlocksCount()        { return 167; }
+  public int getPalBytesAddr()          { return 0xEF82 - 167; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 1, 16);  }
+  
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal7.bin");
+  }
+}
