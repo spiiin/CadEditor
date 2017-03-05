fd477eb822375433b2f8b3005922d8ce5f86d4a3
spiiin <sanya.boyko@gmail.com>
2017-03-05 20:13:31 +0300

    btdd levels 1,2

--- CadEditor/settings_battletoads_and_double_dragon/Settings_BattletoadsDoubleDragon-2.cs
+++ CadEditor/settings_battletoads_and_double_dragon/Settings_BattletoadsDoubleDragon-2.cs
@@ -7,9 +7,39 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec(0x2A5BF - 64*2, 1 , 64*7);   }
   public int getScreenWidth()          { return 64; }
   public int getScreenHeight()         { return 7; }
-  public string getBlocksFilename()    { return "battletoads_doubledragon_2.png"; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x2a6c7 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 83; }
+  public int getBigBlocksCount()        { return 83; }
+  public int getPalBytesAddr()          { return 0x2abf7; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump2.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal2.bin");
+  }
 }
