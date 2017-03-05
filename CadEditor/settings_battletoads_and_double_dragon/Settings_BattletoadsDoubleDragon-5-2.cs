81a7b8410e9909c758aa749a17c462e794613763
spiiin <sanya.boyko@gmail.com>
2017-03-05 21:51:04 +0300

    btdd levels 5, 5-2

--- CadEditor/settings_battletoads_and_double_dragon/Settings_BattletoadsDoubleDragon-5-2.cs
+++ CadEditor/settings_battletoads_and_double_dragon/Settings_BattletoadsDoubleDragon-5-2.cs
@@ -4,13 +4,42 @@ using System.Drawing;
 
 public class Data 
 { 
-  public OffsetRec getScreensOffset()  { return new OffsetRec(185778, 1 , 32*8);   }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x2d52b, 1 , 32*8);   }
   public int getScreenWidth()          { return 32; }
   public int getScreenHeight()         { return 8; }
-  public int getBigBlocksCount()       { return 62; }
-  public string getBlocksFilename()    { return "battletoads_doubledragon_5-2.png"; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x2d62b , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 62; }
+  public int getBigBlocksCount()        { return 62; }
+  public int getPalBytesAddr()          { return 0x2da0b; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("ppu_dump5-2.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal5-2.bin");
+  }
 }
