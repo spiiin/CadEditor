e9cb3cd981900861ebb5d62648f892d999516169
spiiin <sanya.boyko@gmail.com>
2017-03-10 04:22:26 +0300

    mickey, levels 5-6

--- CadEditor/settings_mickey_s_adventures_in_numberland/Settings_super_advanced_Number_6.cs
+++ CadEditor/settings_mickey_s_adventures_in_numberland/Settings_super_advanced_Number_6.cs
@@ -7,9 +7,40 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec( 0x0ee06, 1 , 41*30);   }
   public int getScreenWidth()          { return 41; }
   public int getScreenHeight()         { return 30; }
-  public string getBlocksFilename()    { return "mickey_s_adventures_in_numberland_3.png"; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x18E35 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 126; }
+  public int getBigBlocksCount()        { return 126; }
+  public int getPalBytesAddr()          { return 0x1961d; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr3.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal3.bin");
+  }
 }
