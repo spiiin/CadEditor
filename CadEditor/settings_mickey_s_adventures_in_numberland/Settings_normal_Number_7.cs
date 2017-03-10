d3ac87ee49e73eedb2783f3ffacb146297f378e7
spiiin <sanya.boyko@gmail.com>
2017-03-10 15:09:20 +0300

    mm, levels 7-8

--- CadEditor/settings_mickey_s_adventures_in_numberland/Settings_normal_Number_7.cs
+++ CadEditor/settings_mickey_s_adventures_in_numberland/Settings_normal_Number_7.cs
@@ -7,9 +7,40 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec( 0x10fd9, 1 , 64*12);   }
   public int getScreenWidth()          { return 64; }
   public int getScreenHeight()         { return 12; }
-  public string getBlocksFilename()    { return "mickey_s_adventures_in_numberland_4.png"; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x14e13 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 158; }
+  public int getBigBlocksCount()        { return 158; }
+  public int getPalBytesAddr()          { return 0x15801; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr4.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal4.bin");
+  }
 }
