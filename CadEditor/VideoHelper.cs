b85f2a6e4b36e628048b0435925e19577260cef7
spiiin <sanya.boyko@gmail.com>
2017-03-15 02:31:13 +0300

    block no fix for blocks 4x4

--- CadEditor/VideoHelper.cs
+++ CadEditor/VideoHelper.cs
@@ -24,7 +24,7 @@ namespace CadEditor
             using (Graphics g = Graphics.FromImage(source))
             {
                 g.FillRectangle(new SolidBrush(Color.FromArgb(192, 255, 255, 255)), new Rectangle(0, 0, source.Width, source.Height));
-                g.DrawString(String.Format("{0:X}", no), new Font("Arial", 16), Brushes.Red, new Point(0, 0));
+                g.DrawString(String.Format("{0:X}", no), new Font("Arial", source.Width/4), Brushes.Red, new Point(0, 0));
             }
             return source;
         }
