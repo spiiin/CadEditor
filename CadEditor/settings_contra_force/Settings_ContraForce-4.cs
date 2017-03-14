724b8911c8f49e2f399b1d00f79df941a0ba7be1
spiiin <sanya.boyko@gmail.com>
2017-03-14 20:47:24 +0300

    cf, level 4

---
+++ CadEditor/settings_contra_force/Settings_ContraForce-4.cs
@@ -0,0 +1,47 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x1a7aa, 35 , 8*8); }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 8; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x1b06a, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 191; }
+  public int getBigBlocksCount()        { return 191; }
+  public int getPalBytesAddr()          { return 0x1b06a + 191*16; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal4.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr4.bin", videoPageId);
+  }
+}
