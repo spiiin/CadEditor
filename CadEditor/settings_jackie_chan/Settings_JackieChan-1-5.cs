be241b8b97e6b8f8ad8667a6befcf8a338ff9441
spiiin <sanya.boyko@gmail.com>
2017-03-14 18:00:14 +0300

    jackie chan, level 1-5, 1-6, 1-7

---
+++ CadEditor/settings_jackie_chan/Settings_JackieChan-1-5.cs
@@ -0,0 +1,47 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x2a9d, 1, 8*22); }
+  public int getScreenWidth()         { return 8; }
+  public int getScreenHeight()        { return 22; }
+  public bool getScreenVertical()      { return false; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x2617 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 59; }
+  public int getBigBlocksCount()        { return 59; }
+  public int getPalBytesAddr()          { return 0x2617 - 59; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1-4.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1-4.bin");
+  }
+}
