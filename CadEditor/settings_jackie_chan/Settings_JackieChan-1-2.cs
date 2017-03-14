45874b5bb7eeb98d9af59c30f39f1456ebebc771
spiiin <sanya.boyko@gmail.com>
2017-03-14 03:02:09 +0300

    fixes, config for level 1-2

--- CadEditor/settings_jackie_chan/Settings_JackieChan-1-2.cs
+++ CadEditor/settings_jackie_chan/Settings_JackieChan-1-2.cs
@@ -4,9 +4,9 @@ using System.Drawing;
 
 public class Data 
 { 
-  public OffsetRec getScreensOffset() { return new OffsetRec(0x1F8, 1, 6*150); }
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x134c, 1, 6*48); }
   public int getScreenWidth()         { return 6; }
-  public int getScreenHeight()        { return 150; }
+  public int getScreenHeight()        { return 48; }
   public bool getScreenVertical()      { return true; }
   
   public bool isBuildScreenFromSmallBlocks() { return true; }
@@ -19,10 +19,10 @@ public class Data
   public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
   public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
   
-  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x64c , 1  , 0x1000);  }
-  public int getBlocksCount()           { return 196; }
-  public int getBigBlocksCount()        { return 196; }
-  public int getPalBytesAddr()          { return 0x588; }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x148f , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 35; }
+  public int getBigBlocksCount()        { return 35; }
+  public int getPalBytesAddr()          { return 0x148f - 35; }
   public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
   public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
   
@@ -37,15 +37,11 @@ public class Data
   
   public byte[] getVideoChunk(int videoPageId)
   {
-     return Utils.readVideoBankFromFile("ppu_dump1.bin", videoPageId);
+     return Utils.readVideoBankFromFile("chr1-2.bin", videoPageId);
   }
   
   public byte[] getPallete(int palId)
   {
-    var pallete = new byte[] { 
-      0x0f, 0x16, 0x37, 0x18, 0x0f, 0x21, 0x20, 0x11,
-      0x0f, 0x10, 0x00, 0x0c, 0x0f, 0x05, 0x27, 0x17
-    }; 
-    return pallete;
+      return Utils.readBinFile("pal1-2.bin");
   }
 }
