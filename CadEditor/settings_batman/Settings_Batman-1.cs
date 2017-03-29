c76a6b73f17c916575cb96b10575814f1ab8987d
spiiin <sanya.boyko@gmail.com>
2017-03-29 18:51:06 +0300

    build from rom

--- CadEditor/settings_batman/Settings_Batman-1.cs
+++ CadEditor/settings_batman/Settings_Batman-1.cs
@@ -7,10 +7,34 @@ public class Data
   public OffsetRec getScreensOffset()  { return new OffsetRec(0x10917, 6 , 8*15);   }
   public int getScreenWidth()          { return 8; }
   public int getScreenHeight()         { return 15; }
-  public string getBlocksFilename()    { return "batman_1.png"; }
-  public int    getPictureBlocksWidth(){ return 64; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  public OffsetRec getVideoOffset()                  { return new OffsetRec(0x20010, 1 , 0x1000); }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x1001F , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public GetBlocksFunc getBlocksFunc() { return Utils.getBlocksLinear4x2withoutAttrib;}
+  public SetBlocksFunc setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x10, 0x1C, 0x0C, 0x0f, 0x10, 0x17, 0x06,
+      0x0f, 0x10, 0x00, 0x08, 0x0f, 0x0A, 0x04, 0x0C
+    }; 
+    return pallete;
+  }
+    
 }
