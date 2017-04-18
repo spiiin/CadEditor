fec4aa839a9e97d22a11baf42515c6c394766d88
spiiin <sanya.boyko@gmail.com>
2017-04-18 22:57:45 +0300

    build from rom

--- CadEditor/settings_adventure_in_the_magic_kingdoom/Settings_AitMK-1.cs
+++ CadEditor/settings_adventure_in_the_magic_kingdoom/Settings_AitMK-1.cs
@@ -5,9 +5,50 @@ public class Data
   public OffsetRec getScreensOffset()     { return new OffsetRec(0xA890, 4 /*next - castle level too, with other graphics*/, 8*9);   }
   public int getScreenWidth()             { return 8; }
   public int getScreenHeight()            { return 9; }
-  public string getBlocksFilename()       { return "adv_1.png"; }
   
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x4010, 1  , 0x4000); }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x4810, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return Utils.setBigBlocksCapcomDefault;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public ObjRec[] getBlocks(int tileId)
+  {
+    return Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount());
+  }
+  
+  public void setBlocks(int tileId, ObjRec[] blocks)
+  {
+    Utils.writeBlocksToAlignedArrays(blocks, Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount());
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+  }
 }
