c35d414cdd4b8a43a16c30f2b4d8ab0130995340
spiiin <sanya.boyko@gmail.com>
2017-04-18 22:07:51 +0300

    build from rom

--- CadEditor/settings_terminator_2/Settings_Termnator2-1.cs
+++ CadEditor/settings_terminator_2/Settings_Termnator2-1.cs
@@ -4,12 +4,52 @@ using System.Drawing;
 
 public class Data 
 { 
-  public OffsetRec getScreensOffset() { return new OffsetRec(0x1A177 - 160*12, 1, 160*14); }
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x199f7, 1, 160*14); }
   public int getScreenWidth()         { return 160; }
   public int getScreenHeight()        { return 14; }
-  public string getBlocksFilename()   { return "termnator2_1.png"; }
-  
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x1a2b6, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 206; }
+  public int getBigBlocksCount()        { return 206; }
+  public int getPalBytesAddr()          { return 0x1a2b6 + 206*4; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public ObjRec[] getBlocks(int tileId)
+  {
+    return Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount());
+  }
+  
+  public void setBlocks(int tileId, ObjRec[] blocks)
+  {
+    Utils.writeBlocksToAlignedArrays(blocks, Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount());
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+  }
 }
