22e5751a4616698c311640d8a3b48fffaefcb0d1
spiiin <sanya.boyko@gmail.com>
2017-04-18 23:24:50 +0300

    build from rom

--- CadEditor/settings_raf_world/Settings_RafWorld-1.cs
+++ CadEditor/settings_raf_world/Settings_RafWorld-1.cs
@@ -7,9 +7,66 @@ public class Data
   public OffsetRec getScreensOffset() { return new OffsetRec(0x20, 14, 64); }
   public int getScreenWidth()         { return 8; }
   public int getScreenHeight()        { return 8; }
-  public string getBlocksFilename()   { return "raf_world_1.png"; }
   
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x580, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 150; }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x3a0, 1  , 0x1000);  }
+  public int getBigBlocksCount()        { return 120; }
+  public int getPalBytesAddr()          { return 0x580 + 150*4; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return Utils.setBigBlocksCapcomDefault;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public ObjRec[] getBlocks(int tileId)
+  {
+      int count = ConfigScript.getBlocksCount();
+      var bb = Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(tileId), 2, 2, count, false, false);
+      var palAddr = ConfigScript.getPalBytesAddr();
+      for (int i = 0; i < count; i++)
+      {
+          bb[i].palBytes[0] = Globals.romdata[palAddr + i];
+      }
+      return bb;
+  }
+  
+  public void setBlocks(int tileId, ObjRec[] blocksData)
+  {
+    int addr = ConfigScript.getTilesAddr(tileId);
+    int count = ConfigScript.getBlocksCount();
+    var palAddr = ConfigScript.getPalBytesAddr();
+    Utils.writeBlocksLinear(blocksData, Globals.romdata, addr, count, false, false);
+    int realBlocksCount = 128;
+    for (int i = 0; i < realBlocksCount; i++)
+    {
+        Globals.romdata[palAddr + i] = (byte)blocksData[i].palBytes[0];
+    }
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+  }
 }
