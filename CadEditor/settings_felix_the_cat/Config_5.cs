2333a9f21f2879a170904bc634c0e989966682c6
spiiin <sanya.boyko@gmail.com>
2017-05-04 20:28:35 +0300

    felix dumps and configs for level 4, 5

---
+++ CadEditor/settings_felix_the_cat/Config_5.cs
@@ -0,0 +1,11 @@
+using CadEditor;
+using System.Collections.Generic;
+using System.Drawing;
+
+public class Config
+{
+  public string getFileName()      { return "d:/DEV/CadEditor/CadEditor/Felix the Cat (U) [!].nes";                  }
+  public string getDumpName()      { return "d:/DEV/CadEditor/CadEditor/settings_felix_the_cat/dump5.bin";           }
+  public string getConfigName()    { return "d:/DEV/CadEditor/CadEditor/settings_felix_the_cat/Settings_Felix_5.cs"; }
+  public bool showDumpFileField()  { return true;  }
+}
