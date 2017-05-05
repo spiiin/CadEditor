8fefc42485ac1486831b45615ee069c44a2dcfce
spiiin <sanya.boyko@gmail.com>
2017-05-05 13:17:54 +0300

    build from rom, levels 5,6

--- CadEditor/settings_moon_crystal/Settings_MoonCrystal-Level5-Part2.cs
+++ CadEditor/settings_moon_crystal/Settings_MoonCrystal-Level5-Part2.cs
@@ -1,15 +1,44 @@
 using CadEditor;
 using System;
-using System.Drawing;
+using PluginMapEditor;
+//css_include settings_moon_crystal/MoonUtils.cs;
 
 public class Data 
 { 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
   public OffsetRec getScreensOffset()  { return new OffsetRec(0x1d561, 8 , 16*15);   }
   public int getScreenWidth()          { return 16; }
   public int getScreenHeight()         { return 15; }
-  public string getBlocksFilename()    { return "moon_crystal_5_2.png"; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return MoonUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return MoonUtils.getVideoChunk("chr5-2.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x31198, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksLinear2x2withoutAttrib;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public GetPalFunc           getPalFunc()           { return MoonUtils.readPalFromBin("pal5.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo()      { return MoonUtils.makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MoonUtils.loadMapMoonCrystal; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribs; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+  //----------------------------------------------------------------------------
 }
