c95c99a2a25fc867b36122772dd858a5adf0d0cd
spiiin <sanya.boyko@gmail.com>
2017-05-14 21:34:27 +0300

    configs for all levels

---
+++ CadEditor/settings_sd_hero_soukessen/SD_Hero_Soukessen_-_Taose!_Aku_no_Gundan_Stage_7.cs
@@ -0,0 +1,16 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x6312, 1 ,12*256);   }
+  public int getScreenWidth()          { return 12; }
+  public int getScreenHeight()         { return 256; }
+  public string getBlocksFilename()    { return "SD_Hero_Soukessen_-_Taose!_Aku_no_Gundan_7.png"; }
+  public bool getScreenVertical()      { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+}
