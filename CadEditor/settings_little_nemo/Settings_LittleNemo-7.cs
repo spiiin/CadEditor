07ef061ce5fc3923b455a70a0cd211244263d910
spiiin <sanya.boyko@gmail.com>
2017-08-29 19:06:10 +0300

    little nemo levels 7, 8-1, 8-2, 8-3, doc

--- CadEditor/settings_little_nemo/Settings_LittleNemo-7.cs
+++ CadEditor/settings_little_nemo/Settings_LittleNemo-7.cs
@@ -1,9 +1,18 @@
 using CadEditor;
 using System;
-using System.Drawing;
+using System.Collections.Generic;
+//css_include settings_little_nemo/LittleNemoUtils.cs;
 
 public class Data 
 { 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginEditLayout.dll",
+    };
+  }
+  
   public OffsetRec getScreensOffset()  { return new OffsetRec(51472, 30 , 8*8);   }
   public int getScreenWidth()          { return 8; }
   public int getScreenHeight()         { return 8; }
@@ -11,5 +20,19 @@ public class Data
   
   public bool isBigBlockEditorEnabled() { return false; }
   public bool isBlockEditorEnabled()    { return false; }
-  public bool isEnemyEditorEnabled()    { return false; }
+  public bool isEnemyEditorEnabled()    { return true; }
+  
+  public GetObjectsFunc getObjectsFunc()   { return LittleNemoUtils.getObjectsNemo; }
+  public SetObjectsFunc setObjectsFunc()   { return LittleNemoUtils.setObjectsNemo; }
+  public SortObjectsFunc sortObjectsFunc() { return LittleNemoUtils.sortObjectsNemo; }
+  
+  public GetLayoutFunc getLayoutFunc() { return LittleNemoUtils.getLayoutLinearPlusOne; }
+  public SetLayoutFunc setLayoutFunc() { return LittleNemoUtils.setLayoutLinearPlusOne; }
+  public int getPalBytesAddr()         { return 0xd120; }
+  
+  public IList<LevelRec> getLevelRecs() { return levelRecs; }
+  public IList<LevelRec> levelRecs = new List<LevelRec>() 
+  {
+    new LevelRec(0xd15e, 44, 16, 4, 0xD090), 
+  };
 }
