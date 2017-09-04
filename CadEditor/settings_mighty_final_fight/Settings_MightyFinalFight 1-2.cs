310822d967962cfc0b3f4bd0e495874c89ce045b
spiiin <sanya.boyko@gmail.com>
2017-09-05 00:25:27 +0300

    mmf 1-2 capcombase

--- CadEditor/settings_mighty_final_fight/Settings_MightyFinalFight 1-2.cs
+++ CadEditor/settings_mighty_final_fight/Settings_MightyFinalFight 1-2.cs
@@ -1,15 +1,24 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include Settings_CapcomBase.cs;
 
-public class Data 
+public class Data : CapcomBase
 { 
-  public OffsetRec getScreensOffset()  { return new OffsetRec(0x3010, 4 , 8*8);   }
-  public int getScreenWidth()          { return 8; }
-  public int getScreenHeight()         { return 8; }
-  public string getBlocksFilename()    { return "mighty_final_fight_1b.png"; }
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginChrView.dll",
+    };
+  }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x3010  , 4  , 8*8);   }
+  public OffsetRec getPalOffset()       { return new OffsetRec(0x2E8C , 1  , 16);     }
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0x31010, 1  , 0x1000); }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0x20010, 1  , 0x1000); }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x3710 , 1  , 0x4000); }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x3B10 , 1  , 0x4000); }
   
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
 }
