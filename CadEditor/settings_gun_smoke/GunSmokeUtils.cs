e8fd29d0fa02b7e8f0d485d68ac6b4552e805593
spiiin <sanya.boyko@gmail.com>
2017-09-20 19:10:16 +0300

    gun smoke, build from rom

---
+++ CadEditor/settings_gun_smoke/GunSmokeUtils.cs
@@ -0,0 +1,75 @@
+using CadEditor;
+using System;
+using System.Collections.Generic;
+
+public static class GunSmokeUtils 
+{
+  public static ObjRec[] getBlocks(int tileId)
+  {
+      var blocks = new ObjRec[64];
+      for (int i = 0; i < 64; i++)
+      {
+          blocks[i] = new ObjRec(i*4, i*4+2, i*4+1, i*4+3, 0);
+      }
+      return blocks;
+  }
+  
+  public static BigBlock[] getBigBlocks(int bigTileIndex)
+  {
+      int count = ConfigScript.getBigBlocksCount(0);
+      var bigBlocks =  new BigBlockWithPal[count];
+      var bigBlocksAddr = ConfigScript.getBigTilesAddr(0, bigTileIndex);
+      for (int i = 0; i < count; i++)
+      {
+         var bb = new BigBlockWithPal(2, 2);
+         //physics in hi bits
+         bb.indexes[0]  = Globals.romdata[bigBlocksAddr + i * 5 + 0] & 0x3f;
+         bb.indexes[1]  = Globals.romdata[bigBlocksAddr + i * 5 + 1] & 0x3f;
+         bb.indexes[2]  = Globals.romdata[bigBlocksAddr + i * 5 + 2] & 0x3f;
+         bb.indexes[3]  = Globals.romdata[bigBlocksAddr + i * 5 + 3] & 0x3f;
+         int palByte = Globals.romdata[bigBlocksAddr + i * 5 + 4];
+         bb.palBytes[0] = palByte >> 0 & 0x3;
+         bb.palBytes[1] = palByte >> 2 & 0x3;
+         bb.palBytes[2] = palByte >> 4 & 0x3;
+         bb.palBytes[3] = palByte >> 6 & 0x3;
+         bigBlocks[i] = bb;
+      }
+      return bigBlocks;
+  }
+  
+  public static void writeMaskedValue(int address, int block)
+  {
+      int masked = Globals.romdata[address] & 0xC0;
+      Globals.romdata[address] = (byte)(masked | (block&0x3F));
+  }
+  
+  public static void setBigBlocks(int bigTileIndex, BigBlock[] bigBlocks)
+  {
+      var bigBlocksAddr = ConfigScript.getBigTilesAddr(0, bigTileIndex); 
+      for (int i = 0; i < bigBlocks.Length; i++)
+      {
+         var bb = bigBlocks[i] as BigBlockWithPal;
+         writeMaskedValue(bigBlocksAddr + i * 5 + 0, bb.indexes[0]);
+         writeMaskedValue(bigBlocksAddr + i * 5 + 1, bb.indexes[1]);
+         writeMaskedValue(bigBlocksAddr + i * 5 + 2, bb.indexes[2]);
+         writeMaskedValue(bigBlocksAddr + i * 5 + 3, bb.indexes[3]);
+         int palByte = bb.palBytes[0] | bb.palBytes[1] << 2 | bb.palBytes[2]<<4 | bb.palBytes[3]<< 6;
+         Globals.romdata[bigBlocksAddr + i * 5 + 4] = (byte)palByte;
+      }
+  }
+  
+  public static GetPalFunc readPalFromBin(string fname)
+  {
+      return (int _)=> { return Utils.readBinFile(fname); };
+  }
+  
+  public static GetVideoPageAddrFunc fakeVideoAddr()
+  {
+      return (int _)=> { return -1; };
+  }
+  
+  public static GetVideoChunkFunc getVideoChunk(string fname)
+  {
+     return (int _)=> { return Utils.readVideoBankFromFile(fname, 0); };
+  }
+}
--- CadEditor/settings_gun_smoke/Settings_GunSmoke-1.cs
+++ CadEditor/settings_gun_smoke/Settings_GunSmoke-1.cs
@@ -1,22 +1,34 @@
 using CadEditor;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
+//css_include settings_gun_smoke/GunSmokeUtils.cs;
 
 public class Data
 { 
   public OffsetRec getScreensOffset()     { return new OffsetRec(0x410, 1, 8*128);  }
+  public OffsetRec getBigBlocksOffset()   { return new OffsetRec(0x10 , 1, 0x4000); }
   public int getScreenWidth()    { return 8; }
   public int getScreenHeight()   { return 128; }
   public int getBigBlocksCount() { return 77; }
-  public string getBlocksFilename()       { return "gun_smoke_1.png"; }
+  public int getBlocksCount()    { return 64; }
+  //public string getBlocksFilename()       { return "gun_smoke_1.png"; }
   
   public GetBigTileNoFromScreenFunc getBigTileNoFromScreenFunc() { return getBigTileNoFromScreen; }
   public SetBigTileToScreenFunc     setBigTileToScreenFunc()     { return setBigTileToScreen; }
   
-  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
   public bool isBlockEditorEnabled()    { return false; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()          { return GunSmokeUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()             { return GunSmokeUtils.getVideoChunk("chr1.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()             { return null; }
+  public GetBlocksFunc        getBlocksFunc()                 { return GunSmokeUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc()                 { return null; }
+  public GetBigBlocksFunc     getBigBlocksFunc()              { return GunSmokeUtils.getBigBlocks;}
+  public SetBigBlocksFunc     setBigBlocksFunc()              { return GunSmokeUtils.setBigBlocks;}
+  public GetPalFunc           getPalFunc()                    { return GunSmokeUtils.readPalFromBin("pal1.bin"); }
+  public SetPalFunc           setPalFunc()                    { return null;}
   
   public int getBigTileNoFromScreen(int[] screenData, int index)
   {
