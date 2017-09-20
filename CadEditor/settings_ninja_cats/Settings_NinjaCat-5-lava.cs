ca9fcfb2a562429d0a2d0c4d5108157b2a11440d
spiiin <sanya.boyko@gmail.com>
2017-09-20 12:03:34 +0300

    level 5, build from rom

--- CadEditor/settings_ninja_cats/Settings_NinjaCat-5-lava.cs
+++ CadEditor/settings_ninja_cats/Settings_NinjaCat-5-lava.cs
@@ -1,16 +1,32 @@
 using CadEditor;
 using System;
-using System.Drawing;
+using System.Collections.Generic;
+//css_include settings_ninja_cats/NinjaCatUtils.cs;
 
 public class Data 
 { 
-  public OffsetRec getScreensOffset()  { return new OffsetRec(3953, 32 , 8*5);   }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(3953, 21 , 8*5);   }
   public int getScreenWidth()          { return 5; }
   public int getScreenHeight()         { return 8; }
-  public string getBlocksFilename()    { return "ninja_cats_5-1.png"; }
   public bool getScreenVertical()      { return true; }
   
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public int getPalBytesAddr()          { return 0x4C1d; }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x440e , 8   , 0x4000); }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x572A  , 8   , 0x440); }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()          { return NinjaCatUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()             { return NinjaCatUtils.getVideoChunk("chr5.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()             { return null; }
+  public GetBlocksFunc        getBlocksFunc()                 { return NinjaCatUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc()                 { return NinjaCatUtils.setBlocks;}
+  public GetBigBlocksFunc     getBigBlocksFunc()              { return NinjaCatUtils.getBigBlocks;}
+  public SetBigBlocksFunc     setBigBlocksFunc()              { return NinjaCatUtils.setBigBlocks;}
+  public GetPalFunc           getPalFunc()                    { return NinjaCatUtils.readPalFromBin("pal5.bin"); }
+  public SetPalFunc           setPalFunc()                    { return null;}
+  public GetObjectsFunc getObjectsFunc()                      { return NinjaCatUtils.getObjects;  }
+  public SetObjectsFunc setObjectsFunc()                      { return NinjaCatUtils.setObjects;  }
 }
