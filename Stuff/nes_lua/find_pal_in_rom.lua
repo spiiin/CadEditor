592815b1addb2ca518cbeecc3adaef3874be664c
spiiin <sanya.boyko@gmail.com>
2017-09-29 01:17:45 +0300

    return old working version

--- Stuff/nes_lua/find_pal_in_rom.lua
+++ Stuff/nes_lua/find_pal_in_rom.lua
@@ -3,11 +3,18 @@
 --Author: spiiin
 
 ROM_LEN = rom.readbyte(4) * 0x4000 + 0x10
-romdata = rom.readbyterange(0x10, ROM_LEN)
-paldata = ppu.readbyterange(0x3F00, 0x16)
+pal = ppu.readbyterange(0x3F00, 0x16)
 
-v = 0
-repeat
-    v = romdata:find(paldata, v+1)
-    print("Pal address:", v and string.format("%05X",v) or "-----")
-until not v
+for curAddr = 0x10, ROM_LEN-0x10 do
+  gotoNextIter = false
+  for curOffset = 1, 16 do
+    b = rom.readbyte(curAddr + curOffset)
+    if b ~= pal:sub(curOffset,curOffset):byte() then
+      gotoNextIter = true
+      break
+    end
+  end
+  if not gotoNextIter then
+    print ("Pal address:", (string.format("%05X",curAddr+1)))
+  end
+end
