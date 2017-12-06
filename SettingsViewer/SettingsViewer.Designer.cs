32e873876cf06e9c5ca4d6557bcc62cf8e63668a
spiiin <sanya.boyko@gmail.com>
2017-12-06 20:17:13 +0300

    settings viewer project

---
+++ SettingsViewer/SettingsViewer.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace SettingsViewer
+{
+    partial class SettingsViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgConfig = new System.Windows.Forms.PropertyGrid();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btReload = new System.Windows.Forms.Button();
+            this.tbConfigName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ofConfig = new System.Windows.Forms.OpenFileDialog();
+            this.btExecute = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pgConfig
+            // 
+            this.pgConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pgConfig.DisabledItemForeColor = System.Drawing.SystemColors.ControlText;
+            this.pgConfig.Location = new System.Drawing.Point(12, 100);
+            this.pgConfig.Name = "pgConfig";
+            this.pgConfig.Size = new System.Drawing.Size(464, 363);
+            this.pgConfig.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.btReload);
+            this.panel1.Controls.Add(this.tbConfigName);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 82);
+            this.panel1.TabIndex = 1;
+            // 
+            // btReload
+            // 
+            this.btReload.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btReload.Location = new System.Drawing.Point(17, 43);
+            this.btReload.Name = "btReload";
+            this.btReload.Size = new System.Drawing.Size(433, 23);
+            this.btReload.TabIndex = 9;
+            this.btReload.Text = "Reload";
+            this.btReload.UseVisualStyleBackColor = true;
+            this.btReload.Click += new System.EventHandler(this.btReload_Click);
+            // 
+            // tbConfigName
+            // 
+            this.tbConfigName.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbConfigName.Location = new System.Drawing.Point(129, 4);
+            this.tbConfigName.Margin = new System.Windows.Forms.Padding(4);
+            this.tbConfigName.Name = "tbConfigName";
+            this.tbConfigName.Size = new System.Drawing.Size(321, 22);
+            this.tbConfigName.TabIndex = 8;
+            this.tbConfigName.Click += new System.EventHandler(this.tbConfigName_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Config file name:";
+            // 
+            // btExecute
+            // 
+            this.btExecute.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btExecute.Location = new System.Drawing.Point(12, 469);
+            this.btExecute.Name = "btExecute";
+            this.btExecute.Size = new System.Drawing.Size(464, 23);
+            this.btExecute.TabIndex = 2;
+            this.btExecute.Text = "Execute selected";
+            this.btExecute.UseVisualStyleBackColor = true;
+            this.btExecute.Click += new System.EventHandler(this.btExecute_Click);
+            // 
+            // ConfigViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 504);
+            this.Controls.Add(this.btExecute);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pgConfig);
+            this.Name = "ConfigViewer";
+            this.Text = "Config Viewer";
+            this.Load += new System.EventHandler(this.ConfigViewer_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PropertyGrid pgConfig;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tbConfigName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btReload;
+        private System.Windows.Forms.OpenFileDialog ofConfig;
+        private System.Windows.Forms.Button btExecute;
+    }
+}
+
---
+++ SettingsViewer/SettingsViewer.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSScriptLibrary;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace SettingsViewer
+{
+    public partial class SettingsViewer : Form
+    {
+        public SettingsViewer()
+        {
+            InitializeComponent();
+        }
+
+        private void tbConfigName_Click(object sender, EventArgs e)
+        {
+            if (ofConfig.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    tbConfigName.Text = ofConfig.FileName;
+                    pgConfig.SelectedObject = new ConfigGrid(tbConfigName.Text);
+                    ConfigViewer.Properties.Settings.Default.ConfigName = tbConfigName.Text;
+                    ConfigViewer.Properties.Settings.Default.Save();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(String.Format("Invalid CadEditor config file. Error message: {0}", ex.Message), "Open error");
+                    pgConfig.SelectedObject = null;
+                }
+            }
+        }
+
+        private void btReload_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pgConfig.SelectedObject = new ConfigGrid(tbConfigName.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Invalid CadEditor config file. Error message: {0}", ex.Message), "Open error");
+                pgConfig.SelectedObject = null;
+            }
+        }
+
+        private void btExecute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var item = pgConfig.SelectedGridItem;
+                var methodDelegate = (MethodDelegate)item.Value;
+                if (methodDelegate == null)
+                {
+                    MessageBox.Show("null");
+                }
+                else
+                {
+                    var result = methodDelegate.Invoke();
+                    MessageBox.Show((result ?? "null").ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(String.Format("Can't execute selected method. Error message: {0}", ex.Message), "Execute error");
+            }
+        }
+
+        private void ConfigViewer_Load(object sender, EventArgs e)
+        {
+            tbConfigName.Text = ConfigViewer.Properties.Settings.Default.ConfigName;
+        }
+    }
+
+    class ConfigGrid
+    {
+        public ConfigGrid(string filename)
+        {
+            asm = new AsmHelper(CSScript.Load(filename));
+            data = asm.CreateObject("Data");
+        }
+        AsmHelper asm;
+        object data;
+
+        public MethodDelegate getMethod(string funcName /*, params object[] funcParams*/)
+        {
+            try
+            {
+                return asm.GetMethod(data, String.Format("*.{0}", funcName));
+            }
+            catch (Exception) //all exception catch...
+            {
+                return null;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public string GetCurrentMethodNameWithoutGetter(int stacktraceLevel)
+        {
+            StackTrace st = new StackTrace();
+            StackFrame sf = st.GetFrame(stacktraceLevel); 
+
+            var fullName = sf.GetMethod().Name; //return "get_METHODNAME"
+            return fullName.Replace("get_", "");
+        }
+
+        public MethodDelegate get() { return getMethod(GetCurrentMethodNameWithoutGetter(2));} //just shortcut
+
+        [Category("Levels count")]
+        public MethodDelegate getLevelsCount { get { return get(); } }
+        [Category("Offsets")]
+        public MethodDelegate getPalOffset { get { return get(); } }
+        [Category("Offsets")]
+        public MethodDelegate getVideoOffset { get { return get(); } }
+        [Category("Offsets")]
+        public MethodDelegate getVideoObjOffset { get { return get(); } }
+        [Category("Offsets")]
+        public MethodDelegate getBlocksOffset { get { return get(); } }
+        [Category("Offsets")]
+        public MethodDelegate getScreensOffset { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getScreenWidth { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getScreenHeight { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getScreensOffsetsForLevels { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getScreensOffset2 { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getScreenVertical { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getScreenDataStride { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate getWordLen { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate isLittleEndian { get { return get(); } }
+        [Category("Sega graphics")]
+        public MethodDelegate isUseSegaGraphics { get { return get(); } }
+        [Category("Sega graphics")]
+        public MethodDelegate isBlockSize4x4 { get { return get(); } }
+        [Category("Screen params")]
+        public MethodDelegate isBuildScreenFromSmallBlocks { get { return get(); } }
+        [Category("Layers")]
+        public MethodDelegate getLayersCount { get { return get(); } }
+        [Category("Level recs")]
+        public MethodDelegate getLevelRecs { get { return get(); } }
+        [Category("Min/max object params")]
+        public MethodDelegate getMinObjCoordX { get { return get(); } }
+        [Category("Min/max object params")]
+        public MethodDelegate getMinObjCoordY { get { return get(); } }
+        [Category("Min/max object params")]
+        public MethodDelegate getMinObjType { get { return get(); } }
+        [Category("Min/max object params")]
+        public MethodDelegate getMaxObjCoordX { get { return get(); } }
+        [Category("Min/max object params")]
+        public MethodDelegate getMaxObjCoordY { get { return get(); } }
+        [Category("Min/max object params")]
+        public MethodDelegate getMaxObjType { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getBigBlocksHierarchyCount { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getBigBlocksCountHierarchy { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getBigBlocksCount { get { return get(); } }
+        [Category("Offsets")]
+        public MethodDelegate getBigBlocksOffsetHierarchy { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getBigBlocksOffset { get { return get(); } }
+        [Category("NES CHR params")]
+        public MethodDelegate getVideoPageAddrFunc { get { return get(); } }
+        [Category("NES CHR funcs")]
+        public MethodDelegate getVideoChunkFunc { get { return get(); } }
+        [Category("NES CHR funcs")]
+        public MethodDelegate setVideoChunkFunc { get { return get(); } }
+        [Category("BigBlocks funcs")]
+        public MethodDelegate getBigBlocksFuncs { get { return get(); } }
+        [Category("BigBlocks funcs")]
+        public MethodDelegate setBigBlocksFuncs { get { return get(); } }
+        [Category("BigBlocks funcs")]
+        public MethodDelegate getBigBlocksFunc { get { return get(); } }
+        [Category("BigBlocks funcs")]
+        public MethodDelegate setBigBlocksFunc { get { return get(); } }
+        [Category("Sega blocks funcs")]
+        public MethodDelegate getSegaMappingFunc { get { return get(); } }
+        [Category("Sega blocks funcs")]
+        public MethodDelegate setSegaMappingFunc { get { return get(); } }
+        [Category("Blocks funcs")]
+        public MethodDelegate getBlocksFunc { get { return get(); } }
+        [Category("Blocks funcs")]
+        public MethodDelegate setBlocksFunc { get { return get(); } }
+        [Category("Pal funcs")]
+        public MethodDelegate getPalFunc { get { return get(); } }
+        [Category("Pal funcs")]
+        public MethodDelegate setPalFunc { get { return get(); } }
+        [Category("Objects funcs")]
+        public MethodDelegate getObjectsFunc { get { return get(); } }
+        [Category("Objects funcs")]
+        public MethodDelegate setObjectsFunc { get { return get(); } }
+        [Category("Objects funcs")]
+        public MethodDelegate sortObjectsFunc { get { return get(); } }
+        [Category("Layout funcs")]
+        public MethodDelegate getLayoutFunc { get { return get(); } }
+        [Category("Layout funcs")]
+        public MethodDelegate setLayoutFunc { get { return get(); } }
+        [Category("Converst screen to tile funcs")]
+        public MethodDelegate getConvertScreenTileFunc { get { return get(); } }
+        [Category("Converst screen to tile funcs")]
+        public MethodDelegate getBackConvertScreenTileFunc { get { return get(); } }
+        [Category("Converst screen to tile funcs")]
+        public MethodDelegate getBigTileNoFromScreenFunc { get { return get(); } }
+        [Category("Converst screen to tile funcs")]
+        public MethodDelegate setBigTileToScreenFunc { get { return get(); } }
+        [Category("Objects funcs")]
+        public MethodDelegate getObjectDictionaryFunc { get { return get(); } }
+        [Category("Sega back funcs")]
+        public MethodDelegate loadSegaBackFunc { get { return get(); } }
+        [Category("Sega back funcs")]
+        public MethodDelegate saveSegaBackFunc { get { return get(); } }
+        [Category("Sega back params")]
+        public MethodDelegate getSegaBackWidth { get { return get(); } }
+        [Category("Sega back params")]
+        public MethodDelegate getSegaBackHeight { get { return get(); } }
+        [Category("Render objects params")]
+        public MethodDelegate getDrawObjectFunc { get { return get(); } }
+        [Category("Render objects params")]
+        public MethodDelegate getDrawObjectBigFunc { get { return get(); } }
+        [Category("Render objects params")]
+        public MethodDelegate getRenderToMainScreenFunc { get { return get(); } }
+        [Category("Editors enable params")]
+        public MethodDelegate isBigBlockEditorEnabled { get { return get(); } }
+        [Category("Editors enable params")]
+        public MethodDelegate isBlockEditorEnabled { get { return get(); } }
+        [Category("Editors enable params")]
+        public MethodDelegate isEnemyEditorEnabled { get { return get(); } }
+        [Category("Icons hints")]
+        public MethodDelegate getObjTypesPicturesDir { get { return get(); } }
+        [Category("Scrolls params")]
+        public MethodDelegate isShowScrollsInLayout { get { return get(); } }
+        [Category("Scrolls params")]
+        public MethodDelegate getScrollsOffsetFromLayout { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getBlocksCount { get { return get(); } }
+        [Category("Blocks pictures params")]
+        public MethodDelegate getBlocksFilename { get { return get(); } }
+        [Category("Blocks pictures params")]
+        public MethodDelegate getPictureBlocksWidth { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getBlockTypeNames { get { return get(); } }
+        [Category("Groups params")]
+        public MethodDelegate getGroups { get { return get(); } }
+        [Category("Blocks params")]
+        public MethodDelegate getPalBytesAddr { get { return get(); } }
+    }
+}
