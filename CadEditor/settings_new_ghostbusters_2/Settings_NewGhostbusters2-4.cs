5dee3dc07b6aadda6559bc72af3104a96acafe5b
spiiin <sanya.boyko@gmail.com>
2017-12-24 02:34:34 +0300

    settings for new ghostbusters 2, levels 3,4

---
+++ CadEditor/settings_new_ghostbusters_2/Settings_NewGhostbusters2-4.cs
@@ -0,0 +1,61 @@
+using CadEditor;
+using System.Collections.Generic;
+
+public class Data
+{ 
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0, 2  , 0x1000); }
+  public OffsetRec getPalOffset()       { return new OffsetRec(0, 2  , 16);     }
+  public OffsetRec getVideoObjOffset()  { return new OffsetRec(0, 1  , 0x1000); }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0, 1  , 0x4000); }
+  public OffsetRec getScreensOffset()   { return new OffsetRec(0x16dea, 17 , 16*15);   }
+  public int getScreenWidth()    { return 16; }
+  public int getScreenHeight()   { return 15; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x167EA, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 240; }
+  public int getBigBlocksCount()        { return 240; }
+  
+  public GetObjectsFunc getObjectsFunc()   { return null;  }
+  public SetObjectsFunc setObjectsFunc()   { return null;  }
+  
+  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  public static ObjRec[] getBlocks(int tileId)
+  {
+      return Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount());
+  }
+  
+  public static void setBlocks(int tileId, ObjRec[] blocksData)
+  {
+      Utils.writeBlocksToAlignedArrays(blocksData, Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount());
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal4.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+      return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+      return Utils.readVideoBankFromFile("chr4.bin", 0);
+  }
+ 
+}
