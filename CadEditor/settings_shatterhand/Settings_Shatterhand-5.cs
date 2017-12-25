06a9c8612f0fca57c7fd3d03b2236831cc3282a2
spiiin <sanya.boyko@gmail.com>
2017-12-26 02:09:35 +0300

    shatterhand, settings for level 5,6,7

---
+++ CadEditor/settings_shatterhand/Settings_Shatterhand-5.cs
@@ -0,0 +1,51 @@
+using CadEditor;
+using System;
+using System.Collections.Generic;
+//css_include settings_shatterhand/ShatterhandUtils.cs;
+
+public class Data 
+{
+  /*public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginEditLayout.dll",
+    };
+  }*/
+  
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xE607, 10, 8*8);   }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 8; }
+
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xECC7, 1, 0x400); }
+  public int getPalBytesAddr()          { return 0xF293; }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0xEF97, 1, 0x4000);  }
+
+  public int getBigBlocksCount()        { return 128; }
+  public int getBlocksCount()           { return 128; }
+
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return ShatterhandUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return ShatterhandUtils.getVideoChunk("chr5.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return ShatterhandUtils.getBigBlocks; }
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return ShatterhandUtils.setBigBlocks;}
+  public GetBlocksFunc        getBlocksFunc()        { return ShatterhandUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc()        { return ShatterhandUtils.setBlocks;}
+  public GetPalFunc           getPalFunc()           { return ShatterhandUtils.readPalFromBin("pal5.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+  //public GetObjectsFunc       getObjectsFunc()       { return ShatterhandUtils.getObjects; }
+  //public SetObjectsFunc       setObjectsFunc()       { return ShatterhandUtils.setObjects; }
+  //public GetObjectDictionaryFunc getObjectDictionaryFunc() { return ShatterhandUtils.getObjectDictionary; }
+  //public GetLayoutFunc        getLayoutFunc()        { return ShatterhandUtils.getLayoutLinearSH; }
+  //public SetLayoutFunc        setLayoutFunc()        { return ShatterhandUtils.setLayoutLinearSH; }
+  //public IList<LevelRec> getLevelRecs()  { return levelRec; }
+  
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  /*public IList<LevelRec> levelRec = new List<LevelRec>() 
+  {
+    new LevelRec(0x13001, 35, 16, 7, 0x81a3),
+  };*/
+}
---
+++ CadEditor/settings_shatterhand/Settings_Shatterhand-7.cs
@@ -0,0 +1,51 @@
+using CadEditor;
+using System;
+using System.Collections.Generic;
+//css_include settings_shatterhand/ShatterhandUtils.cs;
+
+public class Data 
+{
+  /*public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginEditLayout.dll",
+    };
+  }*/
+  
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xa987 - 64*4, 26, 8*8);   }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 8; }
+
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xaf47, 1, 0x400); }
+  public int getPalBytesAddr()          { return 0xb437; }
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0xb6af, 1, 0x4000);  }
+
+  public int getBigBlocksCount()        { return 256; }
+  public int getBlocksCount()           { return 256; }
+
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return ShatterhandUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return ShatterhandUtils.getVideoChunk("chr7.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return ShatterhandUtils.getBigBlocks; }
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return ShatterhandUtils.setBigBlocks;}
+  public GetBlocksFunc        getBlocksFunc()        { return ShatterhandUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc()        { return ShatterhandUtils.setBlocks;}
+  public GetPalFunc           getPalFunc()           { return ShatterhandUtils.readPalFromBin("pal7.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+  //public GetObjectsFunc       getObjectsFunc()       { return ShatterhandUtils.getObjects; }
+  //public SetObjectsFunc       setObjectsFunc()       { return ShatterhandUtils.setObjects; }
+  //public GetObjectDictionaryFunc getObjectDictionaryFunc() { return ShatterhandUtils.getObjectDictionary; }
+  //public GetLayoutFunc        getLayoutFunc()        { return ShatterhandUtils.getLayoutLinearSH; }
+  //public SetLayoutFunc        setLayoutFunc()        { return ShatterhandUtils.setLayoutLinearSH; }
+  //public IList<LevelRec> getLevelRecs()  { return levelRec; }
+  
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  /*public IList<LevelRec> levelRec = new List<LevelRec>() 
+  {
+    new LevelRec(0x13001, 35, 16, 7, 0x81a3),
+  };*/
+}
