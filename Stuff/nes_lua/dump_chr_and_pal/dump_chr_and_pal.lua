0fc8e0c7778c199d0de0160b78ebeae7a8370d95
spiiin <sanya.boyko@gmail.com>
2018-02-21 11:19:16 +0300

    save nametable also

--- Stuff/nes_lua/dump_chr_and_pal/dump_chr_and_pal.lua
+++ Stuff/nes_lua/dump_chr_and_pal/dump_chr_and_pal.lua
@@ -13,14 +13,16 @@ irqAddr = memory.readwordunsigned(0xFFFE)
 
 function dumpData()
     save("pal.bin", ppu.readbyterange(0x3F00, 0x10))
-    --save("pal2.bin", ppu.readbyterange(0x3F10, 0x10))
+    save("pal2.bin", ppu.readbyterange(0x3F10, 0x10))
     save("chr.bin", ppu.readbyterange(0, 0x1000))
     save("chr2.bin", ppu.readbyterange(0x1000, 0x1000))
     save("ppu.bin", ppu.readbyterange(0x0, 0x4000))
+    save("nameTable0.bin", ppu.readbyterange(0x2000, 0x400));
     print("Dump complete!")
     
     memory.registerexecute(irqAddr, nil)
 end
 
 
-memory.registerexecute(irqAddr, dumpData)
+--memory.registerexecute(irqAddr, dumpData)
+dumpData()
