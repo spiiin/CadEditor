9d795d85d1324afd55cea3916b9eacec93c639ee
spiiin <sanya.boyko@gmail.com>
2018-03-28 21:54:15 +0300

    mickey mouse all levels

--- CadEditor/settings_mickey_mouse/MickeyMouseUtils.cs
+++ CadEditor/settings_mickey_mouse/MickeyMouseUtils.cs
@@ -27,4 +27,19 @@ public static class MickeyUtils
         Globals.romdata[palAddr + i] = (byte)blocksData[i].palBytes[0];
     }
   }
+  
+  public static GetPalFunc readPalFromBin(string[] fname)
+  {
+      return (int x)=> { return Utils.readBinFile(fname[x]); };
+  }
+  
+  public static GetVideoPageAddrFunc fakeVideoAddr()
+  {
+      return (int _)=> { return -1; };
+  }
+  
+  public static GetVideoChunkFunc getVideoChunk(string[] fname)
+  {
+     return (int x)=> { return Utils.readVideoBankFromFile(fname[x], 0); };
+  }
 }
--- CadEditor/settings_mickey_mouse/Settings_MickeyMouse-1.cs
+++ CadEditor/settings_mickey_mouse/Settings_MickeyMouse-1.cs
@@ -3,16 +3,19 @@ using System;
 //css_include settings_mickey_mouse/MickeyMouseUtils.cs;
 public class Data 
 { 
-  public OffsetRec getScreensOffset()  { return new OffsetRec(0xa010, 1, 32*64);   }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xa010, 1, 64*64);   }
   public int getScreenWidth()          { return 64; }
-  public int getScreenHeight()         { return 32; }
+  public int getScreenHeight()         { return 64; }
+  
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0x0 , 4   , 0x1000);  }
+  public OffsetRec getPalOffset  ()     { return new OffsetRec(0x0 , 6   , 16); }
   
   public bool isBigBlockEditorEnabled() { return true; }
   public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
   
-  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
-  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return MickeyUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return MickeyUtils.getVideoChunk(new[] {"chr1.bin", "chr2.bin", "chr3.bin", "chr4.bin"}); }
   public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
   
   public OffsetRec getBlocksOffset()    { return new OffsetRec(0x4389, 1  , 0x1000);  }
@@ -25,22 +28,19 @@ public class Data
   public SetBlocksFunc        setBlocksFunc() { return MickeyUtils.setBlocks;}
   public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
   public SetBigBlocksFunc     setBigBlocksFunc()     { return Utils.setBigBlocksCapcomDefault;}
-  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public GetPalFunc           getPalFunc()           { return MickeyUtils.readPalFromBin(new[] {"pal1.bin", "pal2.bin", "pal3.bin", "pal3-1.bin", "pal4.bin", "pal5.bin"}); }
   public SetPalFunc           setPalFunc()           { return null;}
   
-  //----------------------------------------------------------------------------
-  public byte[] getPallete(int palId)
+  public GetGroupsFunc getGroupsFunc() { return getGroups; }
+  public GroupRec[] getGroups()
   {
-      return Utils.readBinFile("pal1.bin");
-  }
-  
-  public int getVideoAddress(int id)
-  {
-    return -1;
-  }
-  
-  public byte[] getVideoChunk(int videoPageId)
-  {
-     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+    return new GroupRec[] { 
+      new GroupRec("Level 1" , 0,0,0,0, 0x01),
+      new GroupRec("Level 2" , 1,0,0,1, 0x01),
+      new GroupRec("Level 3" , 2,0,0,2, 0x01),
+      new GroupRec("Level 3-flowers", 2,0,0,3, 0x01),
+      new GroupRec("Level 4" , 3,0,0,4, 0x01),
+      new GroupRec("Level 5" , 3,0,0,5, 0x01),
+    };
   }
 }
