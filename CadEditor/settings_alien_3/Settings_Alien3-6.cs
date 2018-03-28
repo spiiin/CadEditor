8d66cf9294280bbc1b713e559792d30a75622114
spiiin <sanya.boyko@gmail.com>
2018-03-29 00:17:00 +0300

    settings for mission 5,6, boss-3

---
+++ CadEditor/settings_alien_3/Settings_Alien3-6.cs
@@ -0,0 +1,32 @@
+using CadEditor;
+using System;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0xB010, 1, 64*64); }
+  public int getScreenWidth()         { return 64; }
+  public int getScreenHeight()        { return 64; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getVideoOffset()   { return new OffsetRec(0x26010, 1, 0x1000); }
+  public OffsetRec getPalOffset()     { return new OffsetRec(0x9395,  1, 16   ); }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x82D0, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 246; }
+  public int getBigBlocksCount()           { return 246; }
+  public int getPalBytesAddr()          { return 0x82D0+246*16; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()         { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()            { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()            { return Utils.setVideoChunk; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  public GetPalFunc           getPalFunc() { return Utils.getPalleteLinear;}
+  public SetPalFunc           setPalFunc() { return Utils.setPalleteLinear;}
+  //-------------------------------------------------------------------------------
+}
