552c9441fce4cd52f11e8c44ba794b605ca175db
spiiin <sanya.boyko@gmail.com>
2018-03-30 23:06:45 +0300

    super c, level 2

---
+++ CadEditor/settings_super_c/Settings_SuperC-2.cs
@@ -0,0 +1,32 @@
+using CadEditor;
+using System;
+//css_include settings_super_c/SuperCUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x2BCF, 13 , 8*8);   }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 8; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getVideoOffset()     { return new OffsetRec(0x0 , 3   , 0x1000);  }
+  public OffsetRec getPalOffset  ()     { return new OffsetRec(0x0 , 3   , 16); }
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SuperCUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SuperCUtils.getVideoChunk(new[] {"chr2_000.bin", "chr2_001.bin", "chr2_002.bin"}); }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x2F4F, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x3e9F; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return SuperCUtils.readPalFromBin(new[] {"pal2.bin", "pal2-2.bin", "pal2-3.bin"}); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
