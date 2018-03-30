b481256918409f8efe4df78a251083495d0f1d98
spiiin <sanya.boyko@gmail.com>
2018-03-30 14:47:28 +0300

    gun smoke, all levels (by lancuster)

---
+++ CadEditor/settings_gun_smoke/Settings_GunSmoke-3.cs
@@ -0,0 +1,51 @@
+using CadEditor;
+using System;
+using System.Collections.Generic;
+//css_include settings_gun_smoke/GunSmokeUtils.cs;
+
+public class Data
+{ 
+  public OffsetRec getScreensOffset()     { return new OffsetRec(0x8410, 1, 8*159);  }
+  public OffsetRec getBigBlocksOffset()   { return new OffsetRec(0x8010 , 1, 0x4000); }
+  public int getScreenWidth()    { return 8; }
+  public int getScreenHeight()   { return 159; }
+  public int getBigBlocksCount() { return 256; }
+  public int getBlocksCount()    { return 256; }
+  
+  public GetBigTileNoFromScreenFunc getBigTileNoFromScreenFunc() { return getBigTileNoFromScreen; }
+  public SetBigTileToScreenFunc     setBigTileToScreenFunc()     { return setBigTileToScreen; }
+  
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()          { return GunSmokeUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()             { return GunSmokeUtils.getVideoChunk("chr3.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()             { return null; }
+  public GetBlocksFunc        getBlocksFunc()                 { return GunSmokeUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc()                 { return null; }
+  public GetBigBlocksFunc     getBigBlocksFunc()              { return GunSmokeUtils.getBigBlocks;}
+  public SetBigBlocksFunc     setBigBlocksFunc()              { return GunSmokeUtils.setBigBlocks;}
+  public GetPalFunc           getPalFunc()                    { return GunSmokeUtils.readPalFromBin("pal3.bin"); }
+  public SetPalFunc           setPalFunc()                    { return null;}
+  
+  public int getBigTileNoFromScreen(int[] screenData, int index)
+  {
+    int w = getScreenWidth();
+    int h = getScreenHeight();
+    int noY = index / w;
+    noY = h - noY - 1;
+    int noX = index % w;
+    return screenData[noY*w + noX];
+  }
+
+  public void setBigTileToScreen(int[] screenData, int index, int value)
+  {
+    int w = getScreenWidth();
+    int h = getScreenHeight();
+    int noY = index / w;
+    noY = h - noY - 1;
+    int noX = index % w;
+    screenData[noY*w + noX] = value;
+  }
+}
