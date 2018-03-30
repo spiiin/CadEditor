d3450b6d850df4d3595a3031f3785a00ab9e8a05
spiiin <sanya.boyko@gmail.com>
2018-03-30 14:59:12 +0300

    settings for batman, all levels (by lancuster)

---
+++ CadEditor/settings_batman/Settings_Batman-2-3.cs
@@ -0,0 +1,71 @@
+using CadEditor;
+using System;
+using System.Drawing;
+using PluginMapEditor;
+
+public class Data 
+{ 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x139d0, 12 , 8*15);   }
+  public int getScreenWidth()          { return 8; }
+  public int getScreenHeight()         { return 15; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return Utils.setVideoChunk; }
+  public OffsetRec getVideoOffset()                  { return new OffsetRec(0x23010, 1 , 0x1000); }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x12880 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public GetBlocksFunc getBlocksFunc() { return Utils.getBlocksLinear4x2withoutAttrib;}
+  public SetBlocksFunc setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public int getPalBytesAddr()          { return 0x14a90; }
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+    var pallete = new byte[] { 
+      0x0f, 0x28, 0x18, 0x08, 0x0f, 0x10, 0x00, 0x08,
+      0x0f, 0x10, 0x18, 0x07, 0x0f, 0x00, 0x17, 0x06
+    }; 
+    return pallete;
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo() { return makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MapUtils.loadMapBatman; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribs; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+
+  public MapInfo[] makeMapsInfo()
+  {
+     var mapsInfo = new MapInfo[getScreensOffset().recCount];
+     int scrSize = ConfigScript.getScreenWidth(0) * ConfigScript.getScreenHeight(0) * ConfigScript.getWordLen();
+     int attrSize = 64;
+     for (int i = 0; i < mapsInfo.Length; i++)
+     {
+         int da = getScreensOffset().beginAddr + scrSize  * i;
+         int aa = getPalBytesAddr() + attrSize * i;
+         mapsInfo[i] = new MapInfo(){ dataAddr = da, palAddr = ConfigScript.palOffset.beginAddr, videoNo = 0, attribsAddr = aa};
+     }
+     return mapsInfo;
+  }
+  //-------------------------------------------------------------------------------------------------------------------
+    
+}
