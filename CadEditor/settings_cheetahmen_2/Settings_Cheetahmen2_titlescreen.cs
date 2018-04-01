f9f87a2ac0f996a58960cbf96a357c3a3a1913cc
spiiin <sanya.boyko@gmail.com>
2018-04-01 18:30:21 +0300

    settings for intro levels

---
+++ CadEditor/settings_cheetahmen_2/Settings_Cheetahmen2_titlescreen.cs
@@ -0,0 +1,34 @@
+using CadEditor;
+using System;
+//css_include settings_cheetahmen_2/CheetahUtils.cs;
+
+public class Data 
+{
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x1457, 1 , 16*16); }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 16; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getVideoOffset()   { return new OffsetRec(0x53010, 1, 0x256); }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x122c, 1  , 0x32);  }
+  public int getBlocksCount()           { return 128; }
+  public int getBigBlocksCount()        { return 128; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()         { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()            { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()            { return Utils.setVideoChunk; }
+  
+  public GetBlocksFunc getBlocksFunc() { return Cheetah.getBlocks;}
+  public SetBlocksFunc setBlocksFunc() { return Cheetah.setBlocks;}
+  public int getPalBytesAddr()         { return 0x13e8; }
+  
+  public GetPalFunc getPalFunc()  { return Utils.getPalleteLinear;}
+  public SetPalFunc setPalFunc()  { return Utils.setPalleteLinear;}
+  public OffsetRec getPalOffset() { return new OffsetRec(0x1557, 1  , 16  ); }
+}
