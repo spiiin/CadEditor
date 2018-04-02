71b6db07840f7521c28d2ae672a23d410ecb7500
spiiin <sanya.boyko@gmail.com>
2018-04-03 00:59:20 +0300

    settings for level 2 3 eyes, increase screen count for level 1-2 (i.e. include 1-3, 1-4)

--- CadEditor/settings_three_eyes_story/Settings_3Eyes-2-3.cs
+++ CadEditor/settings_three_eyes_story/Settings_3Eyes-2-3.cs
@@ -4,28 +4,28 @@ using System.Collections.Generic;
 
 public class Data
 {
-    public OffsetRec getScreensOffset()   { return new OffsetRec(0x14f44 , 14  , 64);      }
+    public OffsetRec getScreensOffset()   { return new OffsetRec(0x1123c , 7  , 64);      }
     public int getScreenWidth()           { return 8; }
     public int getScreenHeight()          { return 8; }
     
-    public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x14dac,  1  , 0x4000);  }
-    public int getBigBlocksCount()        { return 102; }
+    public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x10e8c,  1  , 0x4000);  }
+    public int getBigBlocksCount()        { return 236; }
     public GetBigBlocksFunc     getBigBlocksFunc()     { return ThreeUtils.getBigBlocks; }
     public SetBigBlocksFunc     setBigBlocksFunc()     { return ThreeUtils.setBigBlocks;}
     
-    public OffsetRec getBlocksOffset()    { return new OffsetRec(0x14c9c , 1  , 0x440);   }
-    public int getBlocksCount()           { return 68; }
-    public int getPalBytesAddr()          { return 0x15344; }
+    public OffsetRec getBlocksOffset()    { return new OffsetRec(0x10d18 , 1  , 0x440);   }
+    public int getBlocksCount()           { return 93; }
+    public int getPalBytesAddr()          { return 0x116fc; }
     public GetBlocksFunc        getBlocksFunc()        { return ThreeUtils.getBlocks;}
     public SetBlocksFunc        setBlocksFunc()        { return ThreeUtils.setBlocks;}
     
     public OffsetRec getVideoOffset()                  { return new OffsetRec(0x0 , 1   , 0x1000);  }
     public GetVideoPageAddrFunc getVideoPageAddrFunc() { return ThreeUtils.fakeVideoAddr(); }
-    public GetVideoChunkFunc    getVideoChunkFunc()    { return ThreeUtils.getVideoChunk(new[] {"chr1-2.bin"}); }
+    public GetVideoChunkFunc    getVideoChunkFunc()    { return ThreeUtils.getVideoChunk(new[] {"chr2-3.bin"}); }
     public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
     
     public OffsetRec getPalOffset() { return new OffsetRec(0x0 , 1   , 16); }
-    public GetPalFunc getPalFunc()  { return ThreeUtils.readPalFromBin(new[] {"pal1-2.bin"}); }
+    public GetPalFunc getPalFunc()  { return ThreeUtils.readPalFromBin(new[] {"pal2-3.bin"}); }
     public SetPalFunc setPalFunc()  { return null;}
     
     public bool isBigBlockEditorEnabled() { return true;  }
