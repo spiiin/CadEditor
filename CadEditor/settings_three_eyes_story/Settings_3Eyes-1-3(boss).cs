854d466c166e036a7bc5f243ec6c038e8098d0cc
spiiin <sanya.boyko@gmail.com>
2018-04-03 01:46:54 +0300

    settings for Mitsume ga Tooru, all levels

---
+++ CadEditor/settings_three_eyes_story/Settings_3Eyes-1-3(boss).cs
@@ -0,0 +1,34 @@
+using CadEditor;
+using System.Collections.Generic;
+//css_include settings_three_eyes_story/ThreeUtils.cs;
+
+public class Data
+{
+    public OffsetRec getScreensOffset()   { return new OffsetRec(0x152c4 , 7  , 64);      }
+    public int getScreenWidth()           { return 8; }
+    public int getScreenHeight()          { return 8; }
+    
+    public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x14dac,  1  , 0x4000);  }
+    public int getBigBlocksCount()        { return 102; }
+    public GetBigBlocksFunc     getBigBlocksFunc()     { return ThreeUtils.getBigBlocks; }
+    public SetBigBlocksFunc     setBigBlocksFunc()     { return ThreeUtils.setBigBlocks;}
+    
+    public OffsetRec getBlocksOffset()    { return new OffsetRec(0x14c9c , 1  , 0x440);   }
+    public int getBlocksCount()           { return 68; }
+    public int getPalBytesAddr()          { return 0x15344; }
+    public GetBlocksFunc        getBlocksFunc()        { return ThreeUtils.getBlocks;}
+    public SetBlocksFunc        setBlocksFunc()        { return ThreeUtils.setBlocks;}
+    
+    public OffsetRec getVideoOffset()                  { return new OffsetRec(0x0 , 1   , 0x1000);  }
+    public GetVideoPageAddrFunc getVideoPageAddrFunc() { return ThreeUtils.fakeVideoAddr(); }
+    public GetVideoChunkFunc    getVideoChunkFunc()    { return ThreeUtils.getVideoChunk(new[] {"chr1-3.bin"}); }
+    public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+    
+    public OffsetRec getPalOffset() { return new OffsetRec(0x0 , 1   , 16); }
+    public GetPalFunc getPalFunc()  { return ThreeUtils.readPalFromBin(new[] {"pal1-3.bin"}); }
+    public SetPalFunc setPalFunc()  { return null;}
+    
+    public bool isBigBlockEditorEnabled() { return true;  }
+    public bool isBlockEditorEnabled()    { return true;  }
+    public bool isEnemyEditorEnabled()    { return false; }
+}
---
+++ CadEditor/settings_three_eyes_story/Settings_3Eyes-3-3(c).cs
@@ -0,0 +1,34 @@
+using CadEditor;
+using System.Collections.Generic;
+//css_include settings_three_eyes_story/ThreeUtils.cs;
+
+public class Data
+{ 
+    public OffsetRec getScreensOffset()   { return new OffsetRec(0x123fc , 1  , 128);      }
+    public int getScreenWidth()           { return 8; }
+    public int getScreenHeight()          { return 16; }
+    
+    public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x11bf0,  1  , 0x4000);  }
+    public int getBigBlocksCount()        { return 256; }
+    public GetBigBlocksFunc     getBigBlocksFunc()     { return ThreeUtils.getBigBlocks; }
+    public SetBigBlocksFunc     setBigBlocksFunc()     { return ThreeUtils.setBigBlocks;}
+    
+    public OffsetRec getBlocksOffset()    { return new OffsetRec(0x11ac8 , 1  , 0x1000);   }
+    public int getBlocksCount()           { return 256; }
+    public int getPalBytesAddr()          { return 0x125fc; }
+    public GetBlocksFunc        getBlocksFunc()        { return ThreeUtils.getBlocks;}
+    public SetBlocksFunc        setBlocksFunc()        { return ThreeUtils.setBlocks;}
+    
+    public OffsetRec getVideoOffset()                  { return new OffsetRec(0x0 , 1   , 0x1000);  }
+    public GetVideoPageAddrFunc getVideoPageAddrFunc() { return ThreeUtils.fakeVideoAddr(); }
+    public GetVideoChunkFunc    getVideoChunkFunc()    { return ThreeUtils.getVideoChunk(new[] {"chr3-1.bin"}); }
+    public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+    
+    public OffsetRec getPalOffset() { return new OffsetRec(0x0 , 1   , 16); }
+    public GetPalFunc getPalFunc()  { return ThreeUtils.readPalFromBin(new[] {"pal3-3.bin"}); }
+    public SetPalFunc setPalFunc()  { return null;}
+    
+    public bool isBigBlockEditorEnabled() { return true;  }
+    public bool isBlockEditorEnabled()    { return true;  }
+    public bool isEnemyEditorEnabled()    { return false; }
+}
