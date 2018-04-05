65457d26ffdc94650775656afae2dbd89230e460
spiiin <sanya.boyko@gmail.com>
2018-04-05 18:17:18 +0300

    settings for ninja crusaders, all levels. by lancuster

---
+++ CadEditor/settings_ninja_crusaders/Settings_NinjaCrusaders-3.cs
@@ -0,0 +1,82 @@
+using CadEditor;
+using System;
+using System.Drawing;
+using PluginMapEditor;
+
+public class Data 
+{ 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x12010, 16 , 24*8); }
+  public int getScreenWidth()          { return 24; }
+  public int getScreenHeight()         { return 8; }
+  public bool getScreenVertical()      { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x12c10, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x13010; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return getBlocksLinear4x1withoutAttrib;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal3.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr3.bin", videoPageId);
+  }
+  
+  public static ObjRec[] getBlocksLinear4x1withoutAttrib(int blockIndex)
+  {
+      return Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(blockIndex), 4, 1, ConfigScript.getBlocksCount(), false, false);
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo()      { return makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MapUtils.loadMapNinjaCrusaders; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribsNinjaCrusaders; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+
+  public MapInfo[] makeMapsInfo()
+  {
+     var mapsInfo = new MapInfo[getScreensOffset().recCount];
+     int scrSize = ConfigScript.getScreenWidth(0) * ConfigScript.getScreenHeight(0) * ConfigScript.getWordLen();
+     int attrSize = 48;
+     for (int i = 0; i < mapsInfo.Length; i++)
+     {
+         int da = getScreensOffset().beginAddr + scrSize  * i;
+         int aa =  getPalBytesAddr() + attrSize * i;
+         mapsInfo[i] = new MapInfo(){ dataAddr = da, palAddr = ConfigScript.palOffset.beginAddr, videoNo = 0, attribsAddr = aa};
+     }
+     return mapsInfo;
+  }
+}
