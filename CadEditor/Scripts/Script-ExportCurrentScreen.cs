f4f467bec1e4776604149a9d8325dd090c4711f4
spiiin <sanya.boyko@gmail.com>
2018-04-05 13:55:01 +0300

    script for export current screen to binary file

---
+++ CadEditor/Scripts/Script-ExportCurrentScreen.cs
@@ -0,0 +1,29 @@
+using CadEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+public class Script
+{
+    public void Execute(FormScript formScript)
+    {
+        formScript.writeLog();
+        formScript.writeLog("Script for export current screen to binary array (one byte per index)");
+        
+        var formMain = formScript.getFormMain();
+        formScript.writeLog();
+        
+        int currentScreenNo = formMain.ScreenNo;
+        formScript.writeLog(String.Format("Current active screen: {0}", currentScreenNo));
+        var activeScreen = formMain.Layers[0].screens[currentScreenNo];
+        var data = activeScreen.Select(x=>(byte)x).ToArray();
+        var fname = ConfigScript.ConfigDirectory + String.Format("screen{0}.bin", currentScreenNo);
+        using (FileStream f = File.OpenWrite(fname))
+        {
+           f.Write(data, 0, data.Length);
+        }
+        formScript.writeLog(String.Format("Screen exported to file: {0}", fname));
+    }
+}
