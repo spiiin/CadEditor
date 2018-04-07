52a96cce5ad7b999f2869222ee0679aa10db113f
spiiin <sanya.boyko@gmail.com>
2018-04-07 14:25:36 +0300

    darkman, settings for levels 1-2, 1-3

---
+++ CadEditor/settings_darkman/Settings_Darkman-1-2.cs
@@ -0,0 +1,34 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x146f0, 5 , 7*40);   }
+  public int getScreenWidth()          { return 40; }
+  public int getScreenHeight()         { return 7; }
+  
+  public bool getScreenVertical()      { return true; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getVideoOffset()   { return new OffsetRec(0x2c010, 1, 0x1000); }
+  public OffsetRec getPalOffset()     { return new OffsetRec(0x70a9, 16, 16   ); }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()         { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()            { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()            { return Utils.setVideoChunk; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x14110 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x14010; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  public GetPalFunc           getPalFunc() { return Utils.getPalleteLinear;}
+  public SetPalFunc           setPalFunc() { return Utils.setPalleteLinear;}
+}
---
+++ CadEditor/settings_darkman/Settings_Darkman-1-3.cs
@@ -0,0 +1,32 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xab60, 1 , 72*7);   }
+  public int getScreenWidth()          { return 72; }
+  public int getScreenHeight()         { return 7; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getVideoOffset()   { return new OffsetRec(0x21010, 1, 0x1000); }
+  public OffsetRec getPalOffset()     { return new OffsetRec(0x70e9, 16, 16   ); }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc()         { return Utils.getChrAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()            { return Utils.getVideoChunk; }
+  public SetVideoChunkFunc    setVideoChunkFunc()            { return Utils.setVideoChunk; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xa338 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0xa238; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  public GetPalFunc           getPalFunc() { return Utils.getPalleteLinear;}
+  public SetPalFunc           setPalFunc() { return Utils.setPalleteLinear;}
+}
