908d323cb4e99d3cfbb7e8c97428104de8053fd8
spiiin <sanya.boyko@gmail.com>
2018-04-07 13:44:55 +0300

    settings for low g man, level 1

---
+++ CadEditor/settings_low_g_man/LowGManUtils.cs
@@ -0,0 +1,44 @@
+using CadEditor;
+using System;
+
+public class LowGManUtils 
+{
+  public static GetPalFunc readPalFromBin(string fname)
+  {
+      return (int _)=> { return Utils.readBinFile(fname); };
+  }
+  
+  public static GetVideoPageAddrFunc fakeVideoAddr()
+  {
+      return (int _)=> { return -1; };
+  }
+  
+  public static GetVideoChunkFunc getVideoChunk(string fname)
+  {
+     return (int _)=> { return Utils.readVideoBankFromFile(fname, 0); };
+  }
+
+  public static ObjRec[] getBlocks(int tileId)
+  {
+      int count = ConfigScript.getBlocksCount();
+      var bb = Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(tileId), 2, 2, count, false, false);
+      var palAddr = ConfigScript.getPalBytesAddr();
+      for (int i = 0; i < count; i++)
+      {
+          bb[i].palBytes[0] = Globals.romdata[palAddr + i];
+      }
+      return bb;
+  }
+  
+  public static void setBlocks(int tileId, ObjRec[] blocksData)
+  {
+    int addr = ConfigScript.getTilesAddr(tileId);
+    int count = ConfigScript.getBlocksCount();
+    var palAddr = ConfigScript.getPalBytesAddr();
+    Utils.writeBlocksLinear(blocksData, Globals.romdata, addr, count, false, false);
+    for (int i = 0; i < count; i++)
+    {
+        Globals.romdata[palAddr + i] = (byte)blocksData[i].palBytes[0];
+    }
+  }
+}
