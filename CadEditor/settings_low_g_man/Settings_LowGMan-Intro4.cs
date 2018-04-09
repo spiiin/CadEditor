f8e7e19541d727e4fdeca6049d1ac9a1697aaf33
spiiin <sanya.boyko@gmail.com>
2018-04-09 13:55:49 +0300

    settings low g man, all levels. by lancuster

---
+++ CadEditor/settings_low_g_man/Settings_LowGMan-Intro4.cs
@@ -0,0 +1,31 @@
+using CadEditor;
+using System;
+using PluginMapEditor;
+//css_include settings_low_g_man/LowGManUtils.cs;
+
+public class Data 
+{  
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x384f, 2 , 16*5);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 5; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return LowGManUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return LowGManUtils.getVideoChunk("chr-intro1.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x3a2f, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x3c3f; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return LowGManUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return LowGManUtils.setBlocks;}
+  public GetPalFunc           getPalFunc()           { return LowGManUtils.readPalFromBin("pal-intro4.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
