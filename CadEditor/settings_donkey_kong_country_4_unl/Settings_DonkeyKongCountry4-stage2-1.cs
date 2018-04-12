1e8633d1e314b602979eefa4e3d69d75bc904a52
spiiin <sanya.boyko@gmail.com>
2018-04-12 21:38:21 +0300

    settings for donkey kong 4 (unlicensed)

---
+++ CadEditor/settings_donkey_kong_country_4_unl/Settings_DonkeyKongCountry4-stage2-1.cs
@@ -0,0 +1,55 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x10010, 26, 16*16); }
+  public int getScreenWidth()         { return 16; }
+  public int getScreenHeight()        { return 16; }
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x11a10, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x11dff + 256*4; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public ObjRec[] getBlocks(int tileId)
+  {
+    return Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount(), false);
+  }
+  
+  public void setBlocks(int tileId, ObjRec[] blocks)
+  {
+    Utils.writeBlocksToAlignedArrays(blocks, Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount(), true, false);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal2-1.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr2-1.bin", videoPageId);
+  }
+}
