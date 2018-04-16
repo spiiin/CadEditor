0dfcdc565566965eb75daa69fa4879a25d6bf454
spiiin <sanya.boyko@gmail.com>
2018-04-16 13:08:54 +0300

    additional rendering at scanlines, where is midframe writing

--- Stuff/nes_lua/mesen_chrRamWriteCounter.lua
+++ Stuff/nes_lua/mesen_chrRamWriteCounter.lua
@@ -9,6 +9,8 @@ writeToChr1 = 0;
 writedBetweenFrames0 = 0;
 writedBetweenFrames1 = 0;
 
+colorCode = 0x4000FF00;
+
 function onStartFrame()
     --old frame ended, and new started.
     --we can measure byte counts writen only between frames
@@ -20,6 +22,13 @@ function onEndFrame()
     emu.log("Frame ended, chr0 writes: "..writeToChr0.." (between frames:"..writedBetweenFrames0..")"..", chr1 writes: "..writeToChr1.." (between frames:"..writedBetweenFrames1..")");
     writeToChr0 = 0;
     writeToChr1 = 0;
+end
+
+function highlightScanline()
+    local state = emu.getState();
+    local color = colorCode + state.ppu.scanline;
+    --emu.log("scanline: "..tostring(state.ppu.scanline));
+    emu.drawLine(0, state.ppu.scanline, 256, state.ppu.scanline, color, 1);
 end
 
 function onChrWrite(address, value)
@@ -34,5 +43,6 @@ end
 emu.addEventCallback(onStartFrame, emu.eventType.startFrame);
 emu.addEventCallback(onEndFrame, emu.eventType.endFrame);
 emu.addMemoryCallback(onChrWrite, emu.memCallbackType.ppuWrite, 0x0000, 0x1FFF);
+emu.addMemoryCallback(highlightScanline, emu.memCallbackType.ppuWrite, 0x0000, 0x3FFF);
 
 emu.displayMessage("Script", "Script loaded.")
