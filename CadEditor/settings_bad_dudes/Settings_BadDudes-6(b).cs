b737989095efa90c7007ddeaf36330fb52f2946a
spiiin <sanya.boyko@gmail.com>
2018-04-23 17:23:07 +0300

    settings for bad dudes, all levels

---
+++ CadEditor/settings_bad_dudes/Settings_BadDudes-6(b).cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+//css_include settings_bad_dudes/BadDudesUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x90ba, 1 , 16*16);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 16; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x9cfa, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 89; }
+  public int getBigBlocksCount()        { return 89; }
+  public int getPalBytesAddr()          { return 0x9e5e; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BadDudesUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return BadDudesUtils.setBlocks;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal6.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr6.bin", videoPageId);
+  }
+}
