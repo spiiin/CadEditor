b44cf29934652a9f7bbaf75ec3b657a84fce4f79
spiiin <sanya.boyko@gmail.com>
2018-04-23 18:49:03 +0300

    dragon the (unl), all levels, by lancuster

---
+++ CadEditor/settings_dragon_the_unl/Settings_DragonThe-8.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+using PluginMapEditor;
+//css_include settings_dragon_the_unl/DragonUtils.cs;
+
+public class Data 
+{
+  /*public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }*/
+  
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x1174d, 10 , 16*12);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 12; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return DragonUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return DragonUtils.getVideoChunk("chr8.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x120cd, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x10dc5; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksLinear2x2withoutAttrib;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public GetPalFunc           getPalFunc()           { return DragonUtils.readPalFromBin("pal8.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo()      { return DragonUtils.makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return DragonUtils.loadMap; }
+  public SaveMapFunc getSaveMapFunc() { return DragonUtils.saveAttribsT; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+  //----------------------------------------------------------------------------
+}
