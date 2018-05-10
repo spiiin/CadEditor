ec72ab994898524368bd8aab8576fdf0f752007a
spiiin <sanya.boyko@gmail.com>
2018-05-10 18:10:17 +0300

    settings for insector x, all levels, by lancuster

---
+++ CadEditor/settings_insector_x/Settings_InsectorX-Round5-1.cs
@@ -0,0 +1,59 @@
+using CadEditor;
+using System;
+using PluginMapEditor;
+//css_include shared_settings/SharedUtils.cs;
+
+public class Data 
+{
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
+  
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xe550, 6 , 16*13+96);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 13; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr5-1.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xe030, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 30; }
+  public int getBigBlocksCount()        { return 30; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksLinear2x2withoutAttrib;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin("pal5-1.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo() { return makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MapUtils.loadMapBatman; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribs; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+
+  public MapInfo[] makeMapsInfo()
+  {
+     var mapsInfo = new MapInfo[getScreensOffset().recCount];
+     int actualScreenSize = ConfigScript.getScreenWidth(0) * ConfigScript.getScreenHeight(0) * ConfigScript.getWordLen();
+     int scrSize = getScreensOffset().recSize;
+     for (int i = 0; i < mapsInfo.Length; i++)
+     {
+         int da = getScreensOffset().beginAddr + scrSize  * i;
+         int aa = da + actualScreenSize + 8;
+         mapsInfo[i] = new MapInfo(){ dataAddr = da, palAddr = ConfigScript.palOffset.beginAddr, videoNo = 0, attribsAddr = aa};
+     }
+     return mapsInfo;
+  }
+  //----------------------------------------------------------------------------
+}
