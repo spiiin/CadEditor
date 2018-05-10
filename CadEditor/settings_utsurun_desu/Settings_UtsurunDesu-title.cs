f4f4b9715c4e5c9289fc25fc55f1feee56631a04
spiiin <sanya.boyko@gmail.com>
2018-05-10 18:43:39 +0300

    settings for utsurun desu, all levels, by lancuster

---
+++ CadEditor/settings_utsurun_desu/Settings_UtsurunDesu-title.cs
@@ -0,0 +1,31 @@
+using CadEditor;
+using System;
+//css_include shared_settings/SharedUtils.cs;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x1676b, 1 , 16*15);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 15; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr-title.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x1624b, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 128; }
+  public int getBigBlocksCount()        { return 128; }
+  public int getPalBytesAddr()          { return 0x1664b; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin("pal-title.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
