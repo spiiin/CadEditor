6c4624609e964871bf0691f1a6b0dde1e8bc830d
spiiin <sanya.boyko@gmail.com>
2018-05-10 18:58:32 +0300

    settings for karnov, all levels, by lancuster

---
+++ CadEditor/settings_karnov/Settings_Karnov-Stage8.cs
@@ -0,0 +1,31 @@
+using CadEditor;
+using System;
+//css_include shared_settings/SharedUtils.cs;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xe058, 20 , 16*12);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 12; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr8.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xf6d8, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 70; }
+  public int getBigBlocksCount()        { return 70; }
+  public int getPalBytesAddr()          { return 0xfad8; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin("pal8.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
