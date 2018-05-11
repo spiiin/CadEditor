a2c517119a043f5ef81aa67af8e872469f3a29ae
spiiin <sanya.boyko@gmail.com>
2018-05-11 15:38:31 +0300

    settings for donald land, all levels, by lancuster

---
+++ CadEditor/settings_donald_land/DonaldLandUtils.cs
@@ -0,0 +1,31 @@
+using CadEditor;
+using System;
+
+public static class DonaldLandUtils 
+{ 
+  public static ObjRec[] getBlocks(int tileId)
+  {
+      int count = ConfigScript.getBlocksCount();
+      var bb = Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(tileId), 2, 2, count, false, false);
+      var palAddr = ConfigScript.getPalBytesAddr();
+      for (int i = 0; i < count; i++)
+      {
+          bb[i].palBytes[0] = (Globals.romdata[palAddr + i*2] & 0x30) >> 4;
+      }
+      return bb;
+  }
+  
+  public static void setBlocks(int tileId, ObjRec[] blocksData)
+  {
+    int addr = ConfigScript.getTilesAddr(tileId);
+    int count = ConfigScript.getBlocksCount();
+    var palAddr = ConfigScript.getPalBytesAddr();
+    Utils.writeBlocksLinear(blocksData, Globals.romdata, addr, count, false, false);
+    for (int i = 0; i < count; i++)
+    {
+        int t = Globals.romdata[palAddr + i*2];
+        t =  t &  0xCF | (blocksData[i].palBytes[0] << 4);
+        Globals.romdata[palAddr + i*2] = (byte)t;
+    }
+  }
+}
---
+++ CadEditor/settings_donald_land/Settings_DonaldLand-12.cs
@@ -0,0 +1,45 @@
+using CadEditor;
+using System;
+//css_include settings_donald_land/DonaldLandUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xa23a, 31 , 16*13);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 13; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xbb6a, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 194; }
+  public int getBigBlocksCount()        { return 194; }
+  public int getPalBytesAddr()          { return 0xbe72; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return DonaldLandUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return DonaldLandUtils.setBlocks;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal12.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr12.bin", videoPageId);
+  }
+}
