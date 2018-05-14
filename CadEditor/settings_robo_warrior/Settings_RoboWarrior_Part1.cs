5f9d517af494be411ae78a337640fc365ca44363
spiiin <sanya.boyko@gmail.com>
2018-05-14 23:21:12 +0300

    settings for robo warrior, all levels, by lancuster

---
+++ CadEditor/settings_robo_warrior/Settings_RoboWarrior_Part1.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x80b0, 77 , 13*16);   }
+  public int getScreenWidth()          { return 13; }
+  public int getScreenHeight()         { return 16; }
+  public bool getScreenVertical()      { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x17c10, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x17b10; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+  }
+}
