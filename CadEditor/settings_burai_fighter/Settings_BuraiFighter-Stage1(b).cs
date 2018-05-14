a60bb6f3c2cdd856d34548048d02b2b118046072
spiiin <sanya.boyko@gmail.com>
2018-05-14 23:47:18 +0300

    settings burai fighter, all levels, by lancuster

---
+++ CadEditor/settings_burai_fighter/Settings_BuraiFighter-Stage1(b).cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x4ea, 8, 8*7); }
+  public int getScreenWidth()         { return 8; }
+  public int getScreenHeight()        { return 7; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x7e2 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 82; }
+  public int getBigBlocksCount()        { return 82; }
+  public int getPalBytesAddr()          { return 0x25c; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1.bin");
+  }
+}
