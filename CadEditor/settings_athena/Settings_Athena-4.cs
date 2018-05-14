757941656f8100eff08360989476f43eeeaacaaa
spiiin <sanya.boyko@gmail.com>
2018-05-14 13:31:26 +0300

    settings for athena, all levels, by lancuster

---
+++ CadEditor/settings_athena/Settings_Athena-4.cs
@@ -0,0 +1,31 @@
+using CadEditor;
+using System;
+//css_include shared_settings/SharedUtils.cs;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x5a49, 10 , 16*13);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 13; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr3.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x2a68, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 253; }
+  public int getBigBlocksCount()        { return 253; }
+  public int getPalBytesAddr()          { return 0x3590; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin(new[] {"pal4.bin"}); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
