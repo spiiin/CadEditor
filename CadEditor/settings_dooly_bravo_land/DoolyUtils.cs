b7d36a47b5b833f2109ef78a97e94ff4377e4572
spiiin <sanya.boyko@gmail.com>
2018-05-14 13:56:22 +0300

    settings for dooly bravo land, all levels, by lancuster

---
+++ CadEditor/settings_dooly_bravo_land/DoolyUtils.cs
@@ -0,0 +1,40 @@
+using CadEditor;
+using System;
+using PluginMapEditor;
+
+public class DoolyUtils 
+{
+  public static GetPalFunc readPalFromBin(string fname)
+  {
+      return (int _)=> { return Utils.readBinFile(fname); };
+  }
+  
+  public static GetVideoPageAddrFunc fakeVideoAddr()
+  {
+      return (int _)=> { return -1; };
+  }
+  
+  public static GetVideoChunkFunc getVideoChunk(string fname)
+  {
+     return (int _)=> { return Utils.readVideoBankFromFile(fname, 0); };
+  }
+  
+  public static OffsetRec getScreensOffset()
+  {
+    return ConfigScript.screensOffset[0];
+  }
+  
+  public static MapInfo[] makeMapsInfo()
+  {
+     var mapsInfo = new MapInfo[getScreensOffset().recCount];
+     int scrSize = ConfigScript.getScreenWidth(0) * ConfigScript.getScreenHeight(0) * ConfigScript.getWordLen();
+     int attrSize = ConfigScript.getScreenWidth(0)/2 * ConfigScript.getScreenHeight(0)/2;
+     for (int i = 0; i < mapsInfo.Length; i++)
+     {
+         int da = getScreensOffset().beginAddr + scrSize * i;
+         int aa = ConfigScript.getPalBytesAddr() + attrSize * i;
+         mapsInfo[i] = new MapInfo(){ dataAddr = da, palAddr = ConfigScript.palOffset.beginAddr, videoNo = 0, attribsAddr = aa};
+     }
+     return mapsInfo;
+  }
+}
---
+++ CadEditor/settings_dooly_bravo_land/Settings_DoolyBravoLand-5.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+using PluginMapEditor;
+//css_include settings_dooly_bravo_land/DoolyUtils.cs;
+
+public class Data 
+{
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
+  
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x8010, 16 , 16*14);   }
+  public int getScreenWidth()          { return 16; }
+  public int getScreenHeight()         { return 14; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return DoolyUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return DoolyUtils.getVideoChunk("chr5.bin");   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x9350, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 136; }
+  public int getBigBlocksCount()        { return 136; }
+  public int getPalBytesAddr()          { return 0x9750; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksLinear2x2withoutAttrib;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public GetPalFunc           getPalFunc()           { return DoolyUtils.readPalFromBin("pal5.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo()      { return DoolyUtils.makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MapUtils.loadMapBatman; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribs; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+  //----------------------------------------------------------------------------
+}
