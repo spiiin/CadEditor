2bb8d2aafe8c43c2a5d6253ce6e2d3eee484f26e
spiiin <sanya.boyko@gmail.com>
2018-05-14 12:01:02 +0300

    settings time diver, all levels, by lancuster

---
+++ CadEditor/settings_time_diver_eon_man/Settings_TimeDiverEonMan-5-4.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+//css_include settings_time_diver_eon_man/TimeDiverUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x1002c, 1 , 128*12);   }
+  public int getScreenWidth()          { return 128; }
+  public int getScreenHeight()         { return 12; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x5f3e, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x633e; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return TimeDiverUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return TimeDiverUtils.setBlocks;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal5-4.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr5-4.bin", videoPageId);
+  }
+}
