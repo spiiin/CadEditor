b9ab5c92565e4f798d1491435eb6bbb5746e824a
spiiin <sanya.boyko@gmail.com>
2018-05-14 13:04:38 +0300

    settings for takahashi, all levels, by lancuster

---
+++ CadEditor/settings_takahashi_meijins_bug_honey/Settings_TakahashiMeijinsBugHoney-2.cs
@@ -0,0 +1,44 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x6555, 1, 128*13); }
+  public int getScreenWidth()         { return 128; }
+  public int getScreenHeight()        { return 13; }
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x5ad5, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x59d5; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksAlignedWithSeparatePal;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksAlignedWithSeparatePal;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal2.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr2.bin", videoPageId);
+  }
+}
