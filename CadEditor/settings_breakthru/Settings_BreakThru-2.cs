63a43192739f2618831b7e157b551c5612240453
spiiin <sanya.boyko@gmail.com>
2018-05-14 11:39:02 +0300

    settings for breakthru, all levels, by lancuster

---
+++ CadEditor/settings_breakthru/Settings_BreakThru-2.cs
@@ -0,0 +1,30 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+//css_include shared_settings/SharedUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x8767, 19, 16*15); }
+  public int getScreenWidth()         { return 16; }
+  public int getScreenHeight()        { return 15; }
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr2.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x8110, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x8710; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksAlignedWithSeparatePal;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksAlignedWithSeparatePal;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin(new[] {"pal2.bin"}); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
---
+++ CadEditor/settings_breakthru/Settings_BreakThru-5.cs
@@ -0,0 +1,30 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+//css_include shared_settings/SharedUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x1479e, 24, 16*15); }
+  public int getScreenWidth()         { return 16; }
+  public int getScreenHeight()        { return 15; }
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr1.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x14110, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x14710; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksAlignedWithSeparatePal;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksAlignedWithSeparatePal;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin(new[] {"pal5.bin"}); }
+  public SetPalFunc           setPalFunc()           { return null;}
+}
