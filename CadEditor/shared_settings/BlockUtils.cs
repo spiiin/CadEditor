c932c2d984d9f894b988dfc60eeec7ea830f13ba
spiiin <sanya.boyko@gmail.com>
2018-05-14 14:42:37 +0300

    shared utils update

--- CadEditor/shared_settings/BlockUtils.cs
+++ CadEditor/shared_settings/BlockUtils.cs
@@ -3,6 +3,7 @@ using System;
 
 public static class BlockUtils
 { 
+  //-----------------------------------------------------------------------------------------------------------------
   public static ObjRec[] getBlocksLinear2x2Masked(int tileId)
   {
       int count = ConfigScript.getBlocksCount();
@@ -26,6 +27,57 @@ public static class BlockUtils
         int t = Globals.romdata[palAddr + i];
         t =  t &  0xFC | blocksData[i].palBytes[0];
         Globals.romdata[palAddr + i] = (byte)t; //save only pal bits, not physics
+    }
+  }
+  
+  //-----------------------------------------------------------------------------------------------------------------
+  public static ObjRec[] getBlocksLinear2x2MaskedWithAttribs(int tileId)
+  {
+      int count = ConfigScript.getBlocksCount();
+      var bb = Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(tileId), 2, 2, count, true, false);
+      var palAddr = ConfigScript.getPalBytesAddr();
+      for (int i = 0; i < count; i++)
+      {
+          bb[i].palBytes[0] = Globals.romdata[palAddr + i] & 0x3; //get only pal, not physics
+      }
+      return bb;
+  }
+  
+  public static void setBlocksLinear2x2MaskedWithAttribs(int tileId, ObjRec[] blocksData)
+  {
+    int addr = ConfigScript.getTilesAddr(tileId);
+    int count = ConfigScript.getBlocksCount();
+    var palAddr = ConfigScript.getPalBytesAddr();
+    Utils.writeBlocksLinear(blocksData, Globals.romdata, addr, count, true, false);
+    for (int i = 0; i < count; i++)
+    {
+        int t = Globals.romdata[palAddr + i];
+        t =  t &  0xFC | blocksData[i].palBytes[0];
+        Globals.romdata[palAddr + i] = (byte)t; //save only pal bits, not physics
+    }
+  }
+  
+  //-----------------------------------------------------------------------------------------------------------------
+  public static ObjRec[] getBlocksAlignedWithSeparatePal(int tileId)
+  {
+    int count = ConfigScript.getBlocksCount();
+    var bb = Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(tileId), count, false);
+    var palAddr = ConfigScript.getPalBytesAddr();
+    for (int i = 0; i < count; i++)
+    {
+        bb[i].palBytes[0] = Globals.romdata[palAddr + i] & 0x3; //get only pal, not physics
+    }
+    return bb;
+  }
+  
+  public static void setBlocksAlignedWithSeparatePal(int tileId, ObjRec[] blocks)
+  {
+    int count = ConfigScript.getBlocksCount();
+    Utils.writeBlocksToAlignedArrays(blocks, Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount(), false, false);
+    var palAddr = ConfigScript.getPalBytesAddr();
+    for (int i = 0; i < count; i++)
+    {
+        Globals.romdata[palAddr + i] = (byte)blocks[i].palBytes[0];
     }
   }
 }
--- CadEditor/shared_settings/SharedUtils.cs
+++ CadEditor/shared_settings/SharedUtils.cs
@@ -17,4 +17,14 @@ public class SharedUtils
   {
      return (int _)=> { return Utils.readVideoBankFromFile(fname, 0); };
   }
+  
+  public static GetPalFunc readPalFromBin(string[] fname)
+  {
+      return (int x)=> { return Utils.readBinFile(fname[x]); };
+  }
+  
+  public static GetVideoChunkFunc getVideoChunk(string[] fname)
+  {
+     return (int x)=> { return Utils.readVideoBankFromFile(fname[x], 0); };
+  }
 }
