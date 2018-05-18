9b13bed4b3dc901e63978a58fb60be427e188466
spiiin <sanya.boyko@gmail.com>
2018-05-18 10:41:42 +0300

    settings for cowlitz games, all levels, by lancuster

---
+++ CadEditor/settings_cowlitz_gamers_2nd_adventure_demo_pd/Settings_CowlitzGamers2ndAdv-All.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0x1ae5, 18, 16*11); }
+  public int getScreenWidth()         { return 16; }
+  public int getScreenHeight()        { return 11; }
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xf9a2, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0x1a1b; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksAlignedWithSeparatePal;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksAlignedWithSeparatePal;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr.bin", videoPageId);
+  }
+}
