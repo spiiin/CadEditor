4261ea15081aba1b343295c381cb16108de02f2d
spiiin <sanya.boyko@gmail.com>
2018-05-18 11:22:00 +0300

    settings for whomp' em, all levels, by lancuster

---
+++ CadEditor/settings_whomp_em/Settings_WhompEm-2-3.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x8e20, 1 , 48*15);   }
+  public int getScreenWidth()          { return 48; }
+  public int getScreenHeight()         { return 15; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xafd3, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 81; }
+  public int getBigBlocksCount()        { return 81; }
+  public int getPalBytesAddr()          { return 0xb1df; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal2.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr2.bin", videoPageId);
+  }
+}
---
+++ CadEditor/settings_whomp_em/Settings_WhompEm-5-3.cs
@@ -0,0 +1,46 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0xcc40, 1 , 48*15);   }
+  public int getScreenWidth()          { return 48; }
+  public int getScreenHeight()         { return 15; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xefae, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 190; }
+  public int getBigBlocksCount()        { return 190; }
+  public int getPalBytesAddr()          { return 0xf2a6; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal5.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr5.bin", videoPageId);
+  }
+}
