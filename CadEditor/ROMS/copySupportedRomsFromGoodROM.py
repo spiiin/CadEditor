bc1e5354cfd0b9bf5647f665c4713040ef2b2b0f
spiiin <sanya.boyko@gmail.com>
2018-07-11 21:58:38 +0300

    update supported games

--- CadEditor/ROMS/copySupportedRomsFromGoodROM.py
+++ CadEditor/ROMS/copySupportedRomsFromGoodROM.py
@@ -92,6 +92,7 @@ fnames = [
     "Megaman III (U) [!].nes",
     "Megaman IV (U) (PRG0) [!].nes",
     "Megaman V (E) [!].nes",
+    "Metal Storm (U) [!].nes",
     "Mickey Mania 7 (Unl) [!].nes",
     "Mickey Mouse 3 - Yume Fuusen (J).nes",
     "Mickey Mousecapade (U) [!].nes",
