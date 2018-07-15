5341104baf3a34cad6e129ffc7a82630f570974b
spiiin <sanya.boyko@gmail.com>
2018-07-15 22:51:53 +0300

    settings for spelunker 2, all levels, by lancuster

---
+++ CadEditor/settings_spelunker_2_yuusha_heno_chousen/Settings_Spelunker2_4(underground2).cs
@@ -0,0 +1,47 @@
+using CadEditor;
+using System;
+using System.Drawing;
+
+public class Data 
+{ 
+  public OffsetRec getScreensOffset() { return new OffsetRec(0xdf5b, 1, 6*336); }
+  public int getScreenWidth()         { return 6; }
+  public int getScreenHeight()        { return 336; }
+  public bool getScreenVertical()      { return true; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xceb3 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 232; }
+  public int getBigBlocksCount()        { return 232; }
+  public int getPalBytesAddr()          { return 0xcdcb; }
+  public GetBlocksFunc        getBlocksFunc() { return Utils.getBlocksFromTiles16Pal1;}
+  public SetBlocksFunc        setBlocksFunc() { return Utils.setBlocksFromTiles16Pal1;}
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  //----------------------------------------------------------------------------
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr-underground.bin", videoPageId);
+  }
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal-underground2.bin");
+  }
+}
