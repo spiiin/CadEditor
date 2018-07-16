5d8e0fb76083b125c358c4018fa7a174553bb442
spiiin <sanya.boyko@gmail.com>
2018-07-16 19:56:03 +0300

    print statistics for verified files

--- SettingsVerifier/Program.cs
+++ SettingsVerifier/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Text;
 using CadEditor;
 
@@ -7,8 +9,11 @@ namespace SettingsEditor
 {
     class Program
     {
+        private static int totalVerified, totalFailed;
+
         static void Main(string[] args)
         {
+            var counter = 0;
             Console.WriteLine("Checking settings files...");
             string rootDirName = System.IO.Path.GetFullPath(".");
             string[] dirNames = System.IO.Directory.GetDirectories(rootDirName, "settings_*");
@@ -17,10 +22,16 @@ namespace SettingsEditor
                 string[] fileNames = System.IO.Directory.GetFiles(dirName, "Settings_*.cs");
                 foreach (var f in fileNames)
                 {
-                    checkAndPrint(f);
+                    counter++;
+                    if (counter > 0) //change to verify only some part of configs
+                    {
+                        checkAndPrint(f);
+                    }
                 }
             }
             Console.ResetColor();
+            Console.WriteLine("Total verified files: {0}", totalVerified);
+            Console.WriteLine("Total failed files  : {0}", totalFailed);
             Console.WriteLine("Done!");
             Console.ReadLine();
         }
@@ -28,6 +39,14 @@ namespace SettingsEditor
         static void checkAndPrint(string filename)
         {
             bool result = checkFile(filename);
+            if (result)
+            {
+                totalVerified++;
+            }
+            else
+            {
+                totalFailed++;
+            }
             Console.ForegroundColor = result ? ConsoleColor.Green : ConsoleColor.Red;
             Console.WriteLine(result ? "File verified: {0}" : "File not verified: {0}", filename);
         }
