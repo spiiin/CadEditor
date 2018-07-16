c6f5fce6517901353d610084737f94f32d87613b
spiiin <sanya.boyko@gmail.com>
2018-07-17 02:12:03 +0300

    - settings for parasol henbee, fixed (correct blocks begin address) - update docs and supported games list

--- CadEditor/settings_parasol_henbee/Settings_ParasolHenbee-1.cs
+++ CadEditor/settings_parasol_henbee/Settings_ParasolHenbee-1.cs
@@ -1,6 +1,7 @@
 using CadEditor;
 using System;
 using System.Drawing;
+//css_include shared_settings/BlockUtils.cs;
 
 public class Data 
 { 
@@ -16,56 +17,17 @@ public class Data
   
   public bool isBuildScreenFromSmallBlocks() { return true; }
   
-  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xace, 1  , 0x1000);  }
-  public int getBlocksCount()           { return 208; }
-  public int getBigBlocksCount()        { return 208; }
-  public int getPalBytesAddr()          { return 0xe0e; }
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x9CE, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  public int getPalBytesAddr()          { return 0xDCE; }
   
-  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
-  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
   public GetPalFunc           getPalFunc()           { return getPallete;}
   public SetPalFunc           setPalFunc()           { return null;}
   
-  public GetBigTileNoFromScreenFunc getBigTileNoFromScreenFunc() { return getBigTileNoFromScreen; }
-  public SetBigTileToScreenFunc     setBigTileToScreenFunc()     { return setBigTileToScreen; }
   //----------------------------------------------------------------------------
-  public static ObjRec[] getBlocks(int tileId)
-  {
-      int count = ConfigScript.getBlocksCount();
-      var bb = Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(tileId), 2, 2, count, false, false);
-      var palAddr = ConfigScript.getPalBytesAddr();
-      for (int i = 0; i < count; i++)
-      {
-          bb[i].palBytes[0] = Globals.romdata[palAddr + i] & 0x3; //get only pal, not physics
-      }
-      return bb;
-  }
-  
-  public static void setBlocks(int tileId, ObjRec[] blocksData)
-  {
-    int addr = ConfigScript.getTilesAddr(tileId);
-    int count = ConfigScript.getBlocksCount();
-    var palAddr = ConfigScript.getPalBytesAddr();
-    Utils.writeBlocksLinear(blocksData, Globals.romdata, addr, count, false, false);
-    for (int i = 0; i < count; i++)
-    {
-        int t = Globals.romdata[palAddr + i];
-        t =  t &  0xFC | blocksData[i].palBytes[0];
-        Globals.romdata[palAddr + i] = (byte)t; //save only pal bits, not physics
-    }
-  }
-  
-  public static int getBigTileNoFromScreen(int[] screenData, int index)
-  {
-    int tileNo = screenData[index];
-    return tileNo & 0xBF;
-  }
-
-  public static void setBigTileToScreen(int[] screenData, int index, int value)
-  {
-    int oldValue = screenData[index];
-    screenData[index] = (oldValue & 0x40) | (value & 0xBF);
-  }
   
   public byte[] getPallete(int palId)
   {
