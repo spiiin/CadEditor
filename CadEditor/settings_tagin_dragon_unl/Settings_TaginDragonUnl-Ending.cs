f26fe738ffd8c0a6a14e534cd2beafdb77f94f71
spiiin <sanya.boyko@gmail.com>
2018-07-17 10:09:47 +0300

    settings for tagin's dragon, all levels, by lancuster

---
+++ CadEditor/settings_tagin_dragon_unl/Settings_TaginDragonUnl-Ending.cs
@@ -0,0 +1,68 @@
+using CadEditor;
+using System;
+using System.Drawing;
+using PluginMapEditor;
+
+public class Data 
+{ 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x2932, 1 , 16*15, 16, 15);   }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x4a8f , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 104; }
+  public int getBigBlocksCount()        { return 104; }
+  public GetBlocksFunc getBlocksFunc() { return Utils.getBlocksLinear2x2withoutAttrib;}
+  public SetBlocksFunc setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public int getPalBytesAddr()          { return 0x3dd3; }
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal-ending.bin");
+  }
+
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr-ending.bin", videoPageId);
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo() { return makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MapUtils.loadMapBatman; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribs; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+
+  public MapInfo[] makeMapsInfo()
+  {
+     var mapsInfo = new MapInfo[getScreensOffset().recCount];
+     int scrSize = getScreensOffset().width * getScreensOffset().height * ConfigScript.getWordLen();
+     int attrSize = 64;
+     for (int i = 0; i < mapsInfo.Length; i++)
+     {
+         int da = getScreensOffset().beginAddr + scrSize  * i;
+         int aa = getPalBytesAddr() + attrSize * i;
+         mapsInfo[i] = new MapInfo(){ dataAddr = da, palAddr = ConfigScript.palOffset.beginAddr, videoNo = 0, attribsAddr = aa};
+     }
+     return mapsInfo;
+  }
+  //-------------------------------------------------------------------------------------------------------------------
+    
+}
---
+++ CadEditor/settings_tagin_dragon_unl/Settings_TaginDragonUnl-Title.cs
@@ -0,0 +1,68 @@
+using CadEditor;
+using System;
+using System.Drawing;
+using PluginMapEditor;
+
+public class Data 
+{ 
+  public string[] getPluginNames() 
+  {
+    return new string[] 
+    {
+      "PluginMapEditor.dll",
+    };
+  }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x2842, 1 , 16*15, 16, 15);   }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x4876 , 1  , 0x1000);  }
+  public int getBlocksCount()           { return 103; }
+  public int getBigBlocksCount()        { return 103; }
+  public GetBlocksFunc getBlocksFunc() { return Utils.getBlocksLinear2x2withoutAttrib;}
+  public SetBlocksFunc setBlocksFunc() { return Utils.setBlocksLinearWithoutAttrib;}
+  public int getPalBytesAddr()          { return 0x3d13; }
+  
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  //----------------------------------------------------------------------------
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal-title.bin");
+  }
+
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr-title.bin", videoPageId);
+  }
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  public MapInfo[] getMapsInfo() { return makeMapsInfo(); }
+  public LoadMapFunc getLoadMapFunc() { return MapUtils.loadMapBatman; }
+  public SaveMapFunc getSaveMapFunc() { return MapUtils.saveAttribs; }
+  public bool isMapReadOnly()         { return true; }
+  public bool mapEditorSharePallete() { return true; }
+
+  public MapInfo[] makeMapsInfo()
+  {
+     var mapsInfo = new MapInfo[getScreensOffset().recCount];
+     int scrSize = getScreensOffset().width * getScreensOffset().height * ConfigScript.getWordLen();
+     int attrSize = 64;
+     for (int i = 0; i < mapsInfo.Length; i++)
+     {
+         int da = getScreensOffset().beginAddr + scrSize  * i;
+         int aa = getPalBytesAddr() + attrSize * i;
+         mapsInfo[i] = new MapInfo(){ dataAddr = da, palAddr = ConfigScript.palOffset.beginAddr, videoNo = 0, attribsAddr = aa};
+     }
+     return mapsInfo;
+  }
+  //-------------------------------------------------------------------------------------------------------------------
+    
+}
