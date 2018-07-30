5ce3f3aa99112e2a301076022790828ed766f9b0
spiiin <sanya.boyko@gmail.com>
2018-07-30 14:34:23 +0300

    settings for GameBoy Darkwing Duck, level 1 (example of using GameBoy video plugin)

---
+++ CadEditor/settings_gb/darkwing_duck/Settings_DarkwingDuck-1.cs
@@ -0,0 +1,47 @@
+using CadEditor;
+using System;
+//css_include shared_settings/BlockUtils.cs;
+
+public class Data 
+{
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x8453, 20 , 10*8, 10, 8);   }
+  
+  public bool isBigBlockEditorEnabled() { return false; }
+  public bool isBlockEditorEnabled()    { return false; }
+  public bool isEnemyEditorEnabled()    { return false; }
+  
+  public float getDefaultScale() { return 2.0f; }
+  
+  public bool isUseGbGraphics()      { return true; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public bool isBuildScreenFromSmallBlocks() { return true; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xB7E7, 1  , 0x1000);  }
+  public int getBlocksCount()           { return 256; }
+  public int getBigBlocksCount()        { return 256; }
+  
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2WithoutAttribs;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2WithoutAttribs;}
+  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public SetPalFunc           setPalFunc()           { return null;}
+  //----------------------------------------------------------------------------
+  
+  public byte[] getPallete(int palId)
+  {
+      return Utils.readBinFile("pal1.bin");
+  }
+  
+  public int getVideoAddress(int id)
+  {
+    return -1;
+  }
+  
+  public byte[] getVideoChunk(int videoPageId)
+  {
+     return Utils.readVideoBankFromFile("chr1.bin", videoPageId);
+  }
+}
