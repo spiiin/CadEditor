ee4d51d9aa0129b193767b43ef26f9432645148e
spiiin <sanya.boyko@gmail.com>
2018-10-29 19:15:24 +0300

    optimize speed of verification in many times by enabling InMemoryAssembly and disabling caching, add reverification stage for all failed files (for fast checking, that configs are fixed).

--- SettingsVerifier/Program.cs
+++ SettingsVerifier/Program.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using CadEditor;
+using CSScriptLibrary;
 
 namespace SettingsEditor
 {
@@ -11,15 +12,17 @@ namespace SettingsEditor
     {
         private static int totalVerified, totalFailed;
 
-        static void Main(string[] args)
+        private static IList<string> checkFiles(IList<string> filesToCheck)
         {
+            totalVerified = totalFailed = 0;
+            var notVerified = new List<string>();
             var counter = 0;
             Console.WriteLine("Checking settings files...");
             string rootDirName = Path.GetFullPath(".");
             var dirNames = new List<string>();
             dirNames.AddRange(Directory.GetDirectories(rootDirName + "\\settings_nes", "*", SearchOption.AllDirectories));
             dirNames.AddRange(Directory.GetDirectories(rootDirName + "\\settings_smd", "*", SearchOption.AllDirectories));
-            dirNames.AddRange(Directory.GetDirectories(rootDirName + "\\settings_gb" , "*", SearchOption.AllDirectories));
+            dirNames.AddRange(Directory.GetDirectories(rootDirName + "\\settings_gb", "*", SearchOption.AllDirectories));
             dirNames.AddRange(Directory.GetDirectories(rootDirName + "\\settings_gba", "*", SearchOption.AllDirectories));
 
             foreach (var dirName in dirNames)
@@ -27,10 +30,16 @@ namespace SettingsEditor
                 string[] fileNames = Directory.GetFiles(dirName, "Settings_*.cs");
                 foreach (var f in fileNames)
                 {
-                    counter++;
-                    if (counter > 900) //change to verify only some part of configs
+                    if ((filesToCheck == null) || filesToCheck.Contains(f))
                     {
-                        checkAndPrint(f);
+                        counter++;
+                        //if (counter > 500) //change to verify only some part of configs
+                        {
+                            if (!checkAndPrint(f))
+                            {
+                                notVerified.Add(f);
+                            }
+                        }
                     }
                 }
             }
@@ -38,10 +47,31 @@ namespace SettingsEditor
             Console.WriteLine("Total verified files: {0}", totalVerified);
             Console.WriteLine("Total failed files  : {0}", totalFailed);
             Console.WriteLine("Done!");
-            Console.ReadLine();
+            return notVerified;
         }
 
-        static void checkAndPrint(string filename)
+        static void Main(string[] args)
+        {
+            CSScript.CacheEnabled = false; //not need cache for testing
+            CSScript.GlobalSettings.InMemoryAssembly = true;
+
+            var notVerified = checkFiles(null);
+            while (notVerified.Count > 0)
+            {
+                notVerified = checkFiles(notVerified);
+                if (notVerified.Count > 0)
+                {
+                    Console.WriteLine("Press any key to repeat check for all falied files");
+                }
+                else
+                {
+                    Console.WriteLine("All files verified!");
+                }
+                Console.ReadLine();
+            }
+        }
+
+        static bool checkAndPrint(string filename)
         {
             bool result = checkFile(filename);
             if (result)
@@ -54,6 +84,7 @@ namespace SettingsEditor
             }
             Console.ForegroundColor = result ? ConsoleColor.Green : ConsoleColor.Red;
             Console.WriteLine(result ? "File verified: {0}" : "File not verified: {0}", filename);
+            return result;
         }
 
         static bool checkFile(string filename)
