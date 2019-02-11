0a535b52e035e374e7694c9c005b368ca4a5de26
spiiin <sanya.boyko@gmail.com>
2019-02-11 14:45:20 +0300

    level 7 built from rom

--- CadEditor/settings_nes/young_indiana_jones_chronicles/Settings_Level3_8_part_1.cs
+++ CadEditor/settings_nes/young_indiana_jones_chronicles/Settings_Level3_8_part_1.cs
@@ -1,14 +1,29 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include shared_settings/SharedUtils.cs;
+//css_include young_indiana_jones_chronicles/IndyUtils.cs;
 
 public class Data 
 { 
-  public OffsetRec getScreensOffset()  { return new OffsetRec( 0x1b849, 1 , 12*16, 12, 16);   }
+  public OffsetRec getScreensOffset()  { return new OffsetRec(0x1b849, 1 , 12*16, 12, 16);   }
   public bool getScreenVertical()      { return true; }
-  public string getBlocksFilename()    { return "young_indiana_jones_chronicles_1_8.png"; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x1b049, 1  , 0x1000);  }
+  public int getPalBytesAddr()          { return 0x1b909; }
+  public GetBlocksFunc        getBlocksFunc() { return IndyUtils.getBlocksFromTiles16Pal1V;}
+  public SetBlocksFunc        setBlocksFunc() { return IndyUtils.setBlocksFromTiles16Pal1V;}
+  public int getBigBlocksCount() { return 128; }
+  public int getBlocksCount()    { return 128; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr7.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin("pal7.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
 }
