9276ebef5159b42dc505579358577e0f1585c541
spiiin <sanya.boyko@gmail.com>
2019-02-11 14:18:15 +0300

    levels 3-4 built from rom

--- CadEditor/settings_nes/young_indiana_jones_chronicles/Settings_Level2_4_part_2.cs
+++ CadEditor/settings_nes/young_indiana_jones_chronicles/Settings_Level2_4_part_2.cs
@@ -1,14 +1,29 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include shared_settings/SharedUtils.cs;
+//css_include young_indiana_jones_chronicles/IndyUtils.cs;
 
 public class Data 
 { 
   public OffsetRec getScreensOffset()  { return new OffsetRec( 0xf7ce, 1 , 6*32, 6, 32);   }
   public bool getScreenVertical()      { return true; }
-  public string getBlocksFilename()    { return "young_indiana_jones_chronicles_1_4_1.png"; }
+  public bool isBuildScreenFromSmallBlocks() { return true; }
   
   public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+  
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0xeb56 , 1  , 0x1000);  }
+  public int getPalBytesAddr()          { return 0xf6d6; }
+  public GetBlocksFunc        getBlocksFunc() { return IndyUtils.getBlocksFromTiles16Pal1V;}
+  public SetBlocksFunc        setBlocksFunc() { return IndyUtils.setBlocksFromTiles16Pal1V;}
+  public int getBigBlocksCount() { return 124; }
+  public int getBlocksCount()    { return 124; }
+  
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk("chr4.bin");    }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+  
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin("pal42.bin"); }
+  public SetPalFunc           setPalFunc()           { return null;}
 }
