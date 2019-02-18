ea25dd4684fc81d2f129946bb200744f204a4fee
spiiin <sanya.boyko@gmail.com>
2019-02-18 21:48:48 +0300

    using library functions

--- CadEditor/settings_nes/super_city_tank_unl/Settings_SuperCityTank-All.cs
+++ CadEditor/settings_nes/super_city_tank_unl/Settings_SuperCityTank-All.cs
@@ -1,10 +1,9 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include shared_settings/BlockUtils.cs;
 
 public class Data 
-{ 
-  //public OffsetRec getScreensOffset()  { return new OffsetRec(0x1a70, 50 , 11*13, 11, 13);   }
+{
   
   public bool isBigBlockEditorEnabled() { return false; }
   public bool isBlockEditorEnabled()    { return true; }
@@ -21,8 +20,8 @@ public class Data
   public int getBigBlocksCount()        { return 16; }
   public int getPalBytesAddr()          { return 0x19bc; }
   
-  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
-  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksLinear2x2Masked;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksLinear2x2Masked;}
   public GetPalFunc           getPalFunc()           { return getPallete;}
   public SetPalFunc           setPalFunc()           { return null;}
 
@@ -88,31 +87,6 @@ public class Data
     return ans;  
   }
   //----------------------------------------------------------------------------
-  public static ObjRec[] getBlocks(int tileId)
-  {
-      int count = ConfigScript.getBlocksCount(tileId);
-      var bb = Utils.readBlocksLinear(Globals.romdata, ConfigScript.getTilesAddr(tileId), 2, 2, count, false, false);
-      var palAddr = ConfigScript.getPalBytesAddr(tileId);
-      for (int i = 0; i < count; i++)
-      {
-          bb[i].palBytes[0] = Globals.romdata[palAddr + i] & 0x3; //get only pal, not physics
-      }
-      return bb;
-  }
-  
-  public static void setBlocks(int tileId, ObjRec[] blocksData)
-  {
-    int addr = ConfigScript.getTilesAddr(tileId);
-    int count = ConfigScript.getBlocksCount(tileId);
-    var palAddr = ConfigScript.getPalBytesAddr(tileId);
-    Utils.writeBlocksLinear(blocksData, Globals.romdata, addr, count, false, false);
-    for (int i = 0; i < count; i++)
-    {
-        int t = Globals.romdata[palAddr + i];
-        t =  t &  0xFC | blocksData[i].palBytes[0];
-        Globals.romdata[palAddr + i] = (byte)t; //save only pal bits, not physics
-    }
-  }
   
   public byte[] getPallete(int palId)
   {
