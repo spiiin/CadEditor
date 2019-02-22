1c53886cff03846e9d020b1a879bba1f1d7e98ab
spiiin <sanya.boyko@gmail.com>
2019-02-22 16:21:35 +0300

    addams family level kitchen rebuild from blocks

--- CadEditor/settings_nes/addams_family_the_pugsley_s_scavenger_hunt/Settings_kitchen_2.cs
+++ CadEditor/settings_nes/addams_family_the_pugsley_s_scavenger_hunt/Settings_kitchen_2.cs
@@ -1,13 +1,34 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include shared_settings/SharedUtils.cs;
+//css_include addams_family_the_pugsley_s_scavenger_hunt/AddamsUtils.cs;
 
 public class Data 
 { 
   public OffsetRec getScreensOffset()  { return new OffsetRec( 0x02fd2, 1 , 15*7, 15, 7);   }
-  public string getBlocksFilename()    { return "kitchen_1.png"; }
-  
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk(new[] {"chr2.bin"}); }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x3C22, 1, 0x1000); }
+  public int getBlocksCount()           { return 158; }
+  
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x353E, 1, 0x1000); }
+  public int getBigBlocksCount()        { return 256; }
+  
+  public static int getPalBytesAddr()          { return 0x3E9A; }
+
+  public GetBlocksFunc        getBlocksFunc() { return AddamsUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return AddamsUtils.setBlocks;}
+  
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return Utils.setBigBlocksCapcomDefault;}
+  
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin(new[] {"pal2.bin"}); }
+  public SetPalFunc           setPalFunc()           { return null;}
 }
--- CadEditor/settings_nes/addams_family_the_pugsley_s_scavenger_hunt/Settings_kitchen_5.cs
+++ CadEditor/settings_nes/addams_family_the_pugsley_s_scavenger_hunt/Settings_kitchen_5.cs
@@ -1,13 +1,33 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include shared_settings/SharedUtils.cs;
+//css_include addams_family_the_pugsley_s_scavenger_hunt/AddamsUtils.cs;
 
 public class Data 
 { 
   public OffsetRec getScreensOffset()  { return new OffsetRec( 0x03235, 1 , 8*7, 8, 7);   }
-  public string getBlocksFilename()    { return "kitchen_2.png"; }
-  
-  public bool isBigBlockEditorEnabled() { return false; }
-  public bool isBlockEditorEnabled()    { return false; }
+  public bool isBigBlockEditorEnabled() { return true; }
+  public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
+
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk(new[] {"chr2-2.bin"}); }
+  public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
+
+  public OffsetRec getBlocksOffset()    { return new OffsetRec(0x3C22, 1, 0x1000); }
+  public int getBlocksCount()           { return 158; }
+  
+  public OffsetRec getBigBlocksOffset() { return new OffsetRec(0x3866, 1, 0x1000); }
+  public int getBigBlocksCount()        { return 240; }
+  
+  public static int getPalBytesAddr()          { return 0x3E9A; }
+
+  public GetBlocksFunc        getBlocksFunc() { return AddamsUtils.getBlocks;}
+  public SetBlocksFunc        setBlocksFunc() { return AddamsUtils.setBlocks;}
+  
+  public GetBigBlocksFunc     getBigBlocksFunc()     { return Utils.getBigBlocksCapcomDefault;}
+  public SetBigBlocksFunc     setBigBlocksFunc()     { return Utils.setBigBlocksCapcomDefault;}
+  
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin(new[] {"pal2-2.bin"}); }
+  public SetPalFunc           setPalFunc()           { return null;}
 }
