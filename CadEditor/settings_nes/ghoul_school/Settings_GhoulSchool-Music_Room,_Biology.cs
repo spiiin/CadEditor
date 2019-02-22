b094c1fdf57f8723c7c7775ff3728b8eaef00c26
spiiin <sanya.boyko@gmail.com>
2019-02-22 13:31:44 +0300

    using shared functions

--- CadEditor/settings_nes/ghoul_school/Settings_GhoulSchool-Music_Room,_Biology.cs
+++ CadEditor/settings_nes/ghoul_school/Settings_GhoulSchool-Music_Room,_Biology.cs
@@ -1,6 +1,7 @@
 using CadEditor;
 using System;
-using System.Drawing;
+//css_include shared_settings/BlockUtils.cs;
+//css_include shared_settings/SharedUtils.cs;
 
 public class Data 
 { 
@@ -11,8 +12,8 @@ public class Data
   public bool isBlockEditorEnabled()    { return true; }
   public bool isEnemyEditorEnabled()    { return false; }
   
-  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return getVideoAddress; }
-  public GetVideoChunkFunc    getVideoChunkFunc()    { return getVideoChunk;   }
+  public GetVideoPageAddrFunc getVideoPageAddrFunc() { return SharedUtils.fakeVideoAddr(); }
+  public GetVideoChunkFunc    getVideoChunkFunc()    { return SharedUtils.getVideoChunk(new[] {"chr-musicroom.bin"}); }
   public SetVideoChunkFunc    setVideoChunkFunc()    { return null; }
   
   public bool isBuildScreenFromSmallBlocks() { return true; }
@@ -22,34 +23,8 @@ public class Data
   public int getBigBlocksCount()        { return 109; }
   public int getPalBytesAddr()          { return 0x141c5; }
   
-  public GetBlocksFunc        getBlocksFunc() { return getBlocks;}
-  public SetBlocksFunc        setBlocksFunc() { return setBlocks;}
-  public GetPalFunc           getPalFunc()           { return getPallete;}
+  public GetBlocksFunc        getBlocksFunc() { return BlockUtils.getBlocksFromAlignedArrays;}
+  public SetBlocksFunc        setBlocksFunc() { return BlockUtils.setBlocksToAlignedArrays;}
+  public GetPalFunc           getPalFunc()           { return SharedUtils.readPalFromBin(new[] {"pal-musicroom.bin"}); }
   public SetPalFunc           setPalFunc()           { return null;}
-  
-  //----------------------------------------------------------------------------
-  public ObjRec[] getBlocks(int tileId)
-  {
-    return Utils.readBlocksFromAlignedArrays(Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount(tileId), false);
-  }
-  
-  public void setBlocks(int tileId, ObjRec[] blocks)
-  {
-    Utils.writeBlocksToAlignedArrays(blocks, Globals.romdata, ConfigScript.getTilesAddr(tileId), ConfigScript.getBlocksCount(tileId), true, false);
-  }
-  
-  public byte[] getPallete(int palId)
-  {
-      return Utils.readBinFile("pal-musicroom.bin");
-  }
-  
-  public int getVideoAddress(int id)
-  {
-    return -1;
-  }
-  
-  public byte[] getVideoChunk(int videoPageId)
-  {
-     return Utils.readVideoBankFromFile("chr-musicroom.bin", videoPageId);
-  }
 }
